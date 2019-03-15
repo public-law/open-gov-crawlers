e2081a0430a80b8d4f031aa68e572446bc76a207
Robb Shecter <robb@public.law>
2019-03-14 21:29:12 -0700

    Chapter list (#1)
    * rough retrieval
    
    * parsing the chapter info
    
    * return items.Chapter
    
    * docs
    
    * parse_chapter_page
    
    * parse Divisions

--- oar/items.py
+++ oar/items.py
@@ -8,7 +8,15 @@
 import scrapy
 
 
-class OarItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Chapter(scrapy.Item):
+    db_id = scrapy.Field()
+    number = scrapy.Field()
+    name = scrapy.Field()
+    url = scrapy.Field()
+
+
+class Division(scrapy.Item):
+    db_id = scrapy.Field()
+    number = scrapy.Field()
+    name = scrapy.Field()
+    url = scrapy.Field()
--- oar/spiders/secure_sos_state_or_us.py
+++ oar/spiders/secure_sos_state_or_us.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from oar import items
 
 
 class SecureSosStateOrUsSpider(scrapy.Spider):
@@ -8,4 +9,34 @@ class SecureSosStateOrUsSpider(scrapy.Spider):
     start_urls = ["https://secure.sos.state.or.us/oard/ruleSearch.action"]
 
     def parse(self, response):
-        self.logger.info(f"A response from {response.url} just arrived!")
+        for option in response.css("#browseForm option"):
+            db_id = option.xpath("@value").get()
+            if db_id == "-1":  # Ignore the heading
+                continue
+
+            number, name = map(str.strip, option.xpath("text()").get().split("-", 1))
+            chapter = items.Chapter(
+                db_id=db_id,
+                number=number,
+                name=name,
+                url=f"https://secure.sos.state.or.us/oard/displayChapterRules.action?selectedChapter={db_id}",
+            )
+            yield chapter
+
+            request = scrapy.Request(chapter["url"], callback=self.parse_chapter_page)
+            request.meta["chapter"] = chapter
+            yield request
+
+    def parse_chapter_page(self, response):
+        # chapter = response.meta["chapter"]
+        for a in response.css("#accordion > h3 > a"):
+            db_id = a.xpath("@href").get().split("=")[1]
+            number, name = map(str.strip, a.xpath("text()").get().split("-", 1))
+            number = number.split(" ")[1]
+
+            yield items.Division(
+                db_id=db_id,
+                number=number,
+                name=name,
+                url=f"https://secure.sos.state.or.us/oard/displayDivisionRules.action?selectedDivision={db_id}",
+            )
