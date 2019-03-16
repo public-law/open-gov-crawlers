9ae166c8c470a48953f267f51ad69112e7ddfb56
Robb Shecter <robb@public.law>
2019-03-15 18:42:25 -0700

    refactor: docs and naming

--- oar/spiders/secure_sos_state_or_us.py
+++ oar/spiders/secure_sos_state_or_us.py
@@ -28,7 +28,7 @@ class SecureSosStateOrUsSpider(scrapy.Spider):
 
 
     def spider_idle(self, spider):
-        """Schedule a simple request in order to return data"""
+        """Schedule a simple request in order to return the collected data"""
         if self.data_submitted: return
 
         self.crawler.engine.schedule(scrapy.Request('http://neverssl.com/', callback=self.submit_data), spider)
@@ -36,11 +36,23 @@ class SecureSosStateOrUsSpider(scrapy.Spider):
 
 
     def submit_data(self, _):
+        """Simply return the collection of all the scraped data.
+        To be used as a callback.
+        """
         self.data_submitted = True
         return self.oar
 
 
     def parse(self, response):
+        """The primary Scrapy callback to begin scraping.
+        Kick off scraping by parsing the main OAR page.
+        """
+        return self.parse_search_page(response)
+
+
+    def parse_search_page(self, response):
+        """The search page contains a list of Chapters, with the names,
+        numbers, and internal id's."""
         for option in response.css("#browseForm option"):
             db_id = option.xpath("@value").get()
             if db_id == "-1":  # Ignore the heading
@@ -62,12 +74,16 @@ class SecureSosStateOrUsSpider(scrapy.Spider):
             request.meta["chapter"] = chapter
             yield request
 
+
     def parse_chapter_page(self, response):
+        """A Chapter's page contains a hierarchical list of all its Divisions
+        along with their contained Rules.
+        """
         chapter = response.meta["chapter"]
 
-        for a in response.css("#accordion > h3 > a"):
-            db_id = a.xpath("@href").get().split("=")[1]
-            number, name = map(str.strip, a.xpath("text()").get().split("-", 1))
+        for anchor in response.css("#accordion > h3 > a"):
+            db_id = anchor.xpath("@href").get().split("=")[1]
+            number, name = map(str.strip, anchor.xpath("text()").get().split("-", 1))
             number = number.split(" ")[1]
             division = items.Division(
                 kind="Division",
@@ -79,15 +95,3 @@ class SecureSosStateOrUsSpider(scrapy.Spider):
 
             chapter["divisions"].append(division)
 
-            # 1. Find the rules w/in the new division.
-            # 2. Add empty rules to the division.
-            # 3. Create a Pipeline to output the to-be-scraped Rule URLs to a plaintext file.
-            # 4. Create a RuleTextSpider to retrieve just the text of the rules.
-            # 5. This RuleTextSpider can create a second JSON file with a simple JSON lines format.
-            # 6. A script can use both JSON files as input and create a good single file.
-            #
-            # Or... do the final yield in a spider_idle or spider_closed signal handler:
-            # https://docs.scrapy.org/en/latest/topics/signals.html
-            #
-            # Or... just scrape the Rule Contents and yield them as simple key/value pairs to
-            # be included in the "JSON" output. And then post-process into proper JSON.
