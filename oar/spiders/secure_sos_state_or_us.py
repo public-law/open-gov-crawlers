0e78a81b96f91e22b46785c9a1e84308df1911f0
Robb Shecter <robb@public.law>
2019-03-15 18:56:36 -0700

    refactor: re-org

--- oar/spiders/secure_sos_state_or_us.py
+++ oar/spiders/secure_sos_state_or_us.py
@@ -14,38 +14,19 @@ class SecureSosStateOrUsSpider(scrapy.Spider):
 
     def __init__(self):
         super()
+
+        # A flag, set after post-processing is finished, to avoid an infinite
+        # loop.
         self.data_submitted = False
-        # The merged data to return for conversion to a JSON tree
+
+        # The object to return for conversion to a JSON tree. All the parse
+        # methods add their results to this structure.
         self.oar = items.OAR(chapters=[])
 
 
-    @classmethod
-    def from_crawler(cls, crawler, *args, **kwargs):
-        """Register to receive the idle event"""
-        spider = super(SecureSosStateOrUsSpider, cls).from_crawler(crawler, *args, **kwargs)
-        crawler.signals.connect(spider.spider_idle, signal=signals.spider_idle)
-        return spider
-
-
-    def spider_idle(self, spider):
-        """Schedule a simple request in order to return the collected data"""
-        if self.data_submitted: return
-
-        self.crawler.engine.schedule(scrapy.Request('http://neverssl.com/', callback=self.submit_data), spider)
-        raise scrapy.exceptions.DontCloseSpider
-
-
-    def submit_data(self, _):
-        """Simply return the collection of all the scraped data.
-        To be used as a callback.
-        """
-        self.data_submitted = True
-        return self.oar
-
-
     def parse(self, response):
-        """The primary Scrapy callback to begin scraping.
-        Kick off scraping by parsing the main OAR page.
+        """The primary Scrapy callback to begin scraping. Kick off scraping by parsing
+        the main OAR page.
         """
         return self.parse_search_page(response)
 
@@ -95,3 +76,37 @@ class SecureSosStateOrUsSpider(scrapy.Spider):
 
             chapter["divisions"].append(division)
 
+
+
+    #
+    # Output a single object: a JSON tree containing all the scraped data. This
+    # code implements that strategy by registering a signal (event) listener to
+    # execute after all scraping has finished, and the data is collected.
+    #
+
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        """Register to receive the idle event"""
+        spider = super(SecureSosStateOrUsSpider, cls).from_crawler(crawler, *args, **kwargs)
+        crawler.signals.connect(spider.spider_idle, signal=signals.spider_idle)
+        return spider
+
+
+    def spider_idle(self, spider):
+        """Schedule a simple request in order to return the collected data"""
+        if self.data_submitted: return
+
+        null_request = scrapy.Request('http://neverssl.com/', callback=self.submit_data)
+        self.crawler.engine.schedule(null_request, spider)
+        raise scrapy.exceptions.DontCloseSpider
+
+
+    def submit_data(self, _):
+        """Simply return the collection of all the scraped data. Ignore the actual
+        scraped content. I haven't figured out another way to submit the merged
+        results.
+
+        To be used as a callback when the spider is idle (i.e., has finished scraping.)
+        """
+        self.data_submitted = True
+        return self.oar
