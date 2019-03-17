216f45d495f1e1540a0ec4f72ff41a2c4d81de70
Robb Shecter <robb@public.law>
2019-03-16 23:15:58 -0700

    first test passing

--- oar/parsers.py
+++ oar/parsers.py
@@ -2,4 +2,9 @@ from typing import List
 
 
 def statute_meta(text: str) -> List[str]:
-    pass
+    """
+    Parse a statute meta line of text. E.g.,
+    input:  'ORS 181A.235 & ORS 192'
+    output: ['181A.235', 'ORS 192']
+    """
+    return [text]
