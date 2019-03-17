e6e101c5e49832d6ff969684a43e89161ce0e958
Robb Shecter <robb@public.law>
2019-03-16 23:19:59 -0700

    refactor

--- test/parsers_test.py
+++ test/parsers_test.py
@@ -10,4 +10,4 @@ def test_statute_meta_1():
 def test_statute_meta_2():
     raw_text = "ORS 181A.235 & ORS 192"
     expected = ["181A.235", "ORS 192"]
-    pass
+    assert statute_meta(raw_text) == expected
