f25592e9b73b791523a73e90a6d19fa00c4fe3c4
Robb Shecter <robb@public.law>
2019-03-16 23:34:08 -0700

    docs

--- oar/parsers.py
+++ oar/parsers.py
@@ -5,9 +5,10 @@ from typing import List
 def statute_meta(text: str) -> List[str]:
     """
     Parse a statute meta line of text.
+
     For example:
     input:  'ORS 181A.235 & ORS 192'
-    output: ('181A.235', 'ORS 192')
+    output: ['181A.235', 'ORS 192']
     """
     stemmed_text = text.replace("&", "").replace("  ", " ")
     return re.findall(r"ORS [^ ]+", stemmed_text)
