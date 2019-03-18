991428c297bcf674479e937109c2c261fa241f84
Robb Shecter <robb@public.law>
2019-03-18 00:48:35 -0700

    parsers.py docstring style

--- oar/parsers.py
+++ oar/parsers.py
@@ -5,11 +5,10 @@ SEPARATOR = re.compile(r"[,&]")
 
 
 def statute_meta(text: str) -> List[str]:
-    """
-    Parse a statute meta line of text.
+    """Parse a statute meta line of text.
 
     For example:
-    input:  'ORS 181A.235 & ORS 192'
-    output: ['181A.235', 'ORS 192']
+      input:  'ORS 181A.235 & ORS 192'
+      output: ['181A.235', 'ORS 192']
     """
     return [s.strip() for s in SEPARATOR.split(text)]
