c2d734c1ff4dc53bbeb73e223aef75c17682c0d5
Robb Shecter <robb@public.law>
2019-03-21 21:19:12 -0700

    fix: ampersand parsing

--- test/parsers_test.py
+++ test/parsers_test.py
@@ -10,7 +10,7 @@ class TestStatuteMeta:
         assert statute_meta(raw_text) == expected
 
     def test_handles_the_ampersand_separator(self):
-        raw_text = "ORS 181A.235 & ORS 192"
+        raw_text = "ORS 181A.235 &amp; ORS 192"
         expected = ["ORS 181A.235", "ORS 192"]
         assert statute_meta(raw_text) == expected
 
@@ -20,31 +20,31 @@ class TestStatuteMeta:
         assert statute_meta(raw_text) == expected
 
     def test_handles_a_mix_of_ranges_and_single_cites(self):
-        raw_text = "ORS 183.310 - 183.550, 192.660, 243.061 - 243.302 & 292.05"
+        raw_text = "ORS 183.310 - 183.550, 192.660, 243.061 - 243.302 &amp; 292.05"
         expected = ["ORS 183.310 - 183.550", "192.660", "243.061 - 243.302", "292.05"]
         assert statute_meta(raw_text) == expected
 
     def test_handles_citations_to_the_OR_constitution(self):
-        raw_text = "ORS 273.045, 273.775 - 273.79 & OR Const., Art. VIII & Sec. 5"
+        raw_text = "ORS 273.045, 273.775 - 273.79 &amp; OR Const., Art. VIII &amp; Sec. 5"
         expected = ["ORS 273.045", "273.775 - 273.79", "OR Const., Art. VIII", "Sec. 5"]
         assert statute_meta(raw_text) == expected
 
     def test_handles_const_cite_without_comma(self):
-        raw_text = "ORS 407.115, 407.125 & Art. XI-A OR Const."
+        raw_text = "ORS 407.115, 407.125 &amp; Art. XI-A OR Const."
         expected = ["ORS 407.115", "407.125", "Art. XI-A OR Const."]
 
     def test_handles_const_cite_with_comma_after_article(self):
-        raw_text = "OR Const. Art. XV, Sec. 4(4) & ORS 461"
+        raw_text = "OR Const. Art. XV, Sec. 4(4) &amp; ORS 461"
         expected = ["OR Const. Art. XV, Sec. 4(4)", "ORS 461"]
 
 
 @pytest.mark.describe("meta_sections()")
 class TestMetaSections:
     def test_parses_when_all_three_types_are_present(self):
-        raw_text = "<p><b>Statutory/Other Authority:</b> ORS 243.061 - 243.302<br><b>Statutes/Other Implemented:</b> ORS.243.125(1)<br><b>History:</b><br>PEBB 2-2005, f. 7-26-05, cert. ef. 7-29-05<br>PEBB 1-2004, f. &amp; cert. ef. 7-2-04<br>PEBB 1-2003, f. &amp; cert. ef. 12-4-03<br></p>"
+        raw_text = "<p><b>Statutory/Other Authority:</b> ORS 243.061 - 243.302<br><b>Statutes/Other Implemented:</b> ORS.243.125(1)<br><b>History:</b><br>PEBB 2-2005, f. 7-26-05, cert. ef. 7-29-05<br>PEBB 1-2004, f. &amp;amp; cert. ef. 7-2-04<br>PEBB 1-2003, f. &amp;amp; cert. ef. 12-4-03<br></p>"
         expected = {
             "authority": ["ORS 243.061 - 243.302"],
             "implements": ["ORS.243.125(1)"],
-            "history": "PEBB 2-2005, f. 7-26-05, cert. ef. 7-29-05<br>PEBB 1-2004, f. &amp; cert. ef. 7-2-04<br>PEBB 1-2003, f. &amp; cert. ef. 12-4-03",
+            "history": "PEBB 2-2005, f. 7-26-05, cert. ef. 7-29-05<br>PEBB 1-2004, f. &amp;amp; cert. ef. 7-2-04<br>PEBB 1-2003, f. &amp;amp; cert. ef. 12-4-03",
         }
         assert meta_sections(raw_text) == expected
