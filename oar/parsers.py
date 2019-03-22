0539db20fe58402d21c26726bcbb6ce6accdfb4c
Robb Shecter <robb@public.law>
2019-03-21 20:01:56 -0700

    refactor: clean code

--- oar/parsers.py
+++ oar/parsers.py
@@ -4,16 +4,6 @@ from typing import Any, Dict, List
 SEPARATOR = re.compile(r"(?<=\d),|&")
 
 
-def statute_meta(text: str) -> List[str]:
-    """Parse a statute meta line of text.
-
-    For example:
-      input:  'ORS 181A.235 & ORS 192'
-      output: ['ORS 181A.235', 'ORS 192']
-    """
-    return [s.strip() for s in SEPARATOR.split(text)]
-
-
 def meta_sections(text: str) -> Dict[str, Any]:
     authority, implements, history = text.split("<br>", maxsplit=2)
 
@@ -22,6 +12,16 @@ def meta_sections(text: str) -> Dict[str, Any]:
         "implements": statute_meta(implements.split("</b>")[1].strip()),
         "history": _delete_all(history, ["<b>History:</b><br>", "<br></p>"]),
     }
+
+
+def statute_meta(text: str) -> List[str]:
+    """Parse a statute meta line of text.
+
+    For example:
+      input:  'ORS 181A.235 & ORS 192'
+      output: ['ORS 181A.235', 'ORS 192']
+    """
+    return [s.strip() for s in SEPARATOR.split(text)]
 
 
 def _delete_all(text: str, fragments: List[str]) -> str:
