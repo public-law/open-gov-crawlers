c9d4dc46a64a5c011c64a6e750a02927b36483a0
Robb Shecter <robb@public.law>
2020-01-22 18:01:38 -0800

    fix: working

--- oar/settings.py
+++ oar/settings.py
@@ -29,7 +29,7 @@ USER_AGENT = 'Public.Law Parser (https://www.public.law/contact-us)'
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 2
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
@@ -37,7 +37,7 @@ CONCURRENT_REQUESTS = 2
 # DOWNLOAD_DELAY = 3
 DOWNLOAD_TIMEOUT = 600
 # The download delay setting will honor only one of:
-# CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 32
 # CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
@@ -95,8 +95,8 @@ AUTOTHROTTLE_ENABLED = False
 # Enable and configure HTTP caching (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
 # Enabled for development:
-HTTPCACHE_ENABLED = True
-HTTPCACHE_EXPIRATION_SECS = 0
-HTTPCACHE_DIR = "httpcache"
-HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
-HTTPCACHE_POLICY = "scrapy.extensions.httpcache.DummyPolicy"
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = "httpcache"
+# HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+# HTTPCACHE_POLICY = "scrapy.extensions.httpcache.DummyPolicy"
