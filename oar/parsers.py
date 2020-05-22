1e10a80090f92e39305533d19e9864a7bc61d8e5
Robb Shecter <robb@public.law>
2020-05-21 22:48:59 -0600

    rule text pasing

--- oar/parsers.py
+++ oar/parsers.py
@@ -45,11 +45,32 @@ def parse_rule(rule_div: Selector) -> Rule:
     name = rule_div.css('strong::text').get()
     name = name.strip()
 
+    return parse_rule_content(rule_div, number, name)
+
+
+def parse_rule_content(rule_div: Selector, number: str, name: str) -> Rule:
+    raw_paragraphs: List[str] = rule_div.xpath("p")[1:-1].getall()
+    cleaned_up_paragraphs = [
+        p.strip().replace("\n", "") for p in raw_paragraphs
+    ]
+    cleaned_up_paragraphs = [
+        re.sub(r' +', ' ', p) for p in cleaned_up_paragraphs
+    ]
+    non_empty_paragraphs = list(filter(None, cleaned_up_paragraphs))
+    content_paragaphs = non_empty_paragraphs[1:]
+
+    # meta_paragraph = non_empty_paragraphs[-1]
+    # metadata = meta_sections(meta_paragraph)
+
     return Rule(
         kind="Rule",
         number=number,
         name=name,
-        url=oar_url(f"view.action?ruleNumber={number}")
+        url=oar_url(f"view.action?ruleNumber={number}"),
+        text="\n".join(content_paragaphs),
+        # authority=metadata["authority"],
+        # implements=metadata["implements"],
+        # history=metadata["history"]
     )
 
 
