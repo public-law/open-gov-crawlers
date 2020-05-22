917b6e052cc7df66ed7bfa0966f1c89b7b106593
Robb Shecter <robb@public.law>
2020-05-21 19:28:54 -0600

    Optimizations and Fixes (#21)
    * Notes for a big optimization
    
    * remove pylint
    
    * type annotations
    
    * more config updates
    
    * more typing info
    
    * more type annos
    
    * update mypy
    
    * Using the internal URLs
    
    * Better logging
    
    * several bug fixes
    
    * Enable autothrottle, getting 60 pages/minute
    
    * remove travis config

--- oar/settings.py
+++ oar/settings.py
@@ -10,10 +10,9 @@ import os
 #     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
-LOG_LEVEL = "INFO"
+LOG_LEVEL = "DEBUG"
 
 BOT_NAME = "oar"
-
 SPIDER_MODULES = ["oar.spiders"]
 NEWSPIDER_MODULE = "oar.spiders"
 
@@ -32,7 +31,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-# DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 0.25
 
 #
 # Crawlera Best Practices. Current Plan is "C50".
@@ -52,7 +51,9 @@ CONCURRENT_REQUESTS_PER_DOMAIN = 5
 # COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-# TELNETCONSOLE_ENABLED = False
+TELNETCONSOLE_ENABLED = True
+TELNETCONSOLE_USERNAME = 'scrapy'
+TELNETCONSOLE_PASSWORD = 'scrapy'
 
 # Override the default request headers:
 # DEFAULT_REQUEST_HEADERS = {
@@ -89,17 +90,16 @@ CRAWLERA_APIKEY = os.environ['CRAWLERA_KEY']
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
-AUTOTHROTTLE_ENABLED = False
-
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-# AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 1
 # The maximum download delay to be set in case of high latencies
-# AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 10
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 4.0
 # Enable showing throttling stats for every response received:
-# AUTOTHROTTLE_DEBUG = True
+AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
--- oar/spiders/secure_sos_state_or_us.py
+++ oar/spiders/secure_sos_state_or_us.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from datetime import datetime
+from datetime import datetime, date
 import logging
+from oar.items import Chapter
 import pytz
 import scrapy
-from scrapy import signals
+import scrapy.exceptions
+import scrapy.http
+import scrapy.signals
+from scrapy import Selector
 from titlecase import titlecase
+from typing import List
+from typing_extensions import Protocol
+
 
 from oar import items
 from oar import parsers
@@ -16,6 +23,10 @@ URL_PREFIX = f"https://{DOMAIN}/oard/"
 
 def oar_url(relative_fragment: str) -> str:
     return URL_PREFIX + relative_fragment
+
+
+class ParseException(Exception):
+    pass
 
 
 class SecureSosStateOrUsSpider(scrapy.Spider):
@@ -34,84 +45,103 @@ class SecureSosStateOrUsSpider(scrapy.Spider):
         # methods add their results to this structure.
         self.oar = items.OAR(date_accessed=todays_date(), chapters=[])
 
-    def parse(self, response):
+    def parse(self, response: scrapy.http.Response):
         """The primary Scrapy callback to begin scraping.
 
         Kick off scraping by parsing the main OAR page.
         """
         return self.parse_search_page(response)
 
-
-    def parse_search_page(self, response):
+    def parse_search_page(self, response: scrapy.http.Response):
         """Parse the top-level page.
 
         The search page contains a list of Chapters, with the names,
         numbers, and internal id's.
         """
+        option: Selector
         for option in response.css("#browseForm option"):
             db_id = option.xpath("@value").get()
             if db_id == "-1":  # Ignore the heading
                 continue
 
-            number, name = map(str.strip, option.xpath("text()").get().split("-", 1))
+            number, name = map(str.strip, option.xpath(
+                "text()").get().split("-", 1))
             chapter = new_chapter(db_id, number, name)
             self.oar["chapters"].append(chapter)
 
-            request = scrapy.Request(chapter["url"], callback=self.parse_chapter_page)
+            request = scrapy.Request(
+                chapter["url"], callback=self.parse_chapter_page)
             request.meta["chapter"] = chapter
             yield request
 
-
-    def parse_chapter_page(self, response):
+    def parse_chapter_page(self, response: scrapy.http.Response):
         """Parse a mid-level page.
 
         A Chapter's page contains a hierarchical list of all its Divisions
         along with their contained Rules.
         """
-        chapter = response.meta["chapter"]
+        chapter: Chapter = response.meta["chapter"]
         division_index = {}
 
         # Collect the Divisions
+        anchor: Selector
         for anchor in response.css("#accordion > h3 > a"):
-            db_id = anchor.xpath("@href").get().split("=")[1]
+            db_id: str = anchor.xpath("@href").get().split("=")[1]
             raw_number, raw_name = map(
                 str.strip, anchor.xpath("text()").get().split("-", 1)
             )
             number = raw_number.split(" ")[1]
-            name = titlecase(raw_name)
+            name: str = titlecase(raw_name)
             division = new_division(db_id, number, name)
 
             chapter["divisions"].append(division)
             division_index[division.number_in_rule_format()] = division
 
         # Collect empty Rules
-        for anchor in response.css(".rule_div > p"):
-            # TODO: Figure out how to do both of these
-            #       in either css or xpath selectors:
+        for anchor_paragraph in response.css(".rule_div > p"):
+            # TODO: Use the Rule's db_id to generate its URL for scraping.
+            #       Possibly add a second url attribute to Rule, e.g.,
+            #       scraping_url. Meanwhile, the current one is canonical_url.
             try:
-                number = anchor.css("strong > a::text").get().strip()
-                name = anchor.xpath("text()").get().strip()
-                rule = new_rule(number, name)
+                number = anchor_paragraph.css("strong > a::text").get()
+                if number is None:
+                    raise ParseException("Couldn't parse number")
+                number = number.strip()
+
+                name = anchor_paragraph.xpath("text()").get()
+                if name is None:
+                    raise ParseException("Couldn't parse name")
+                name = name.strip()
+
+                internal_path = anchor_paragraph.xpath(
+                    'strong/a').xpath('@href').get()
+                rule = new_rule(number, name, internal_path)
+                logging.debug(
+                    f"Got internal url in Chapter {chapter['number']}:  {rule['internal_url']}")
 
                 # Retrieve the Rule details
-                request = scrapy.Request(rule["url"], callback=self.parse_rule_page)
+                request = scrapy.Request(
+                    rule['internal_url'], callback=self.parse_rule_page)
                 request.meta["rule"] = rule
                 yield request
 
                 # Find its Division and add it
                 parent_division = division_index[rule.division_number()]
                 parent_division["rules"].append(rule)
-            except:
-                logging.info(f"Error parsing anchor: {anchor.get()}")
+            except ParseException as e:
+                logging.warn(
+                    f"Error parsing anchor paragraph: {anchor_paragraph.get()}, in chapter {chapter['number']}: {e}")
 
-    def parse_rule_page(self, response):
+    def parse_rule_page(self, response: scrapy.http.Response):
         """Parse a leaf node (bottom level) page.
 
         The Rule page contains the actual Rule's full text.
         The Rule object has already been created with the remaining info,
         so here we just retrieve the text and save it.
         """
-        raw_paragraphs = response.xpath("//p")[1:-1].getall()
+        logging.debug(
+            f"Parsing rule page... {response.meta['rule']['number']}")
+        raw_paragraphs: List[str] = response.xpath("//p")[1:-1].getall()
         cleaned_up_paragraphs = [
             p.strip().replace("  ", "").replace("\n", "") for p in raw_paragraphs
         ]
@@ -135,11 +165,12 @@ class SecureSosStateOrUsSpider(scrapy.Spider):
 
     @classmethod
     def from_crawler(cls, crawler, *args, **kwargs):
-        """Register to receive the idle event"""
-        spider = super(SecureSosStateOrUsSpider, cls).from_crawler(
+        """Override to register to receive the idle event"""
+        spider: SecureSosStateOrUsSpider = super(SecureSosStateOrUsSpider, cls).from_crawler(
             crawler, *args, **kwargs
         )
-        crawler.signals.connect(spider.spider_idle, signal=signals.spider_idle)
+        crawler.signals.connect(
+            spider.spider_idle, signal=scrapy.signals.spider_idle)
         return spider
 
     def spider_idle(self, spider):
@@ -150,7 +181,8 @@ class SecureSosStateOrUsSpider(scrapy.Spider):
         # This is a hack: I don't yet know how to schedule a request to just
         # submit data _without_ also triggering a scrape. So I provide a URL
         # to a simple site that we're going to ignore.
-        null_request = scrapy.Request("http://neverssl.com/", callback=self.submit_data)
+        null_request = scrapy.Request(
+            "http://neverssl.com/", callback=self.submit_data)
         self.crawler.engine.schedule(null_request, spider)
         raise scrapy.exceptions.DontCloseSpider
 
@@ -187,16 +219,22 @@ def new_division(db_id: str, number: str, name: str):
     )
 
 
-def new_rule(number: str, name: str):
+def new_rule(number: str, name: str, internal_path: str):
     return items.Rule(
         kind="Rule",
         number=number,
         name=name,
         url=oar_url(f"view.action?ruleNumber={number}"),
+        internal_url=f"https://secure.sos.state.or.us{internal_path}"
     )
 
 
+class SimpleTimezone(Protocol):
+    def localize(self, dt: datetime) -> date:
+        ...
+
+
 def todays_date() -> str:
-    pacific = pytz.timezone("US/Pacific")
+    mountain: SimpleTimezone = pytz.timezone("US/Mountain")
     fmt = "%Y-%m-%d"
-    return pacific.localize(datetime.now()).strftime(fmt)
+    return mountain.localize(datetime.now()).strftime(fmt)
