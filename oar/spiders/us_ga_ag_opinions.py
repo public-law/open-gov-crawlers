65c71646ec1d1ac84b28cc20aa67b07671a11db4
Robb Shecter <robb@public.law>
2020-09-08 21:33:49 -0600

    create basic scraping structure

--- oar/spiders/us_ga_ag_opinions.py
+++ oar/spiders/us_ga_ag_opinions.py
@@ -9,4 +9,19 @@ class UsAgGaOpinions(scrapy.Spider):
     producing one JSON record per opinion.
     """
 
-    pass
+    name = "us_ag_ga_opinions"
+    start_urls = [
+        "https://law.georgia.gov/opinions/official",
+        "https://law.georgia.gov/opinions/unofficial",
+    ]
+
+    def parse(self, response):
+        opinion_paths = response.xpath(
+            "//td[contains(@class, 'views-field-title')]/a/@href"
+        ).getall()
+
+        for url in [response.urljoin(p) for p in opinion_paths]:
+            yield scrapy.Request(url, callback=self.parse_opinion)
+
+    def parse_opinion(self, response):
+        yield {"Opinion URL": response.url}
