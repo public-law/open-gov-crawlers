17fb9afc0c4443dc114a4e1aaef3a57d77bd6636
Robb Shecter <robb@public.law>
2020-09-08 19:57:53 -0600

    UsAgOpinions spider

---
+++ oar/spiders/us_ga_ag_opinions.py
@@ -0,0 +1,12 @@
+import scrapy
+
+
+# Nothing at all
+class UsAgGaOpinions(scrapy.Spider):
+    """Scrape the Georgia Attorney General Opinions
+
+    Scrape both the official and unofficial opinions,
+    producing one JSON record per opinion.
+    """
+
+    pass
