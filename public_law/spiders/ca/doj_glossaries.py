bcf86e6b6cd64d6adce630ade65784e094109ccd
Robb Shecter <robb@public.law>
2020-09-19 23:51:06 -0600

    removed bad source

--- public_law/spiders/ca/doj_glossaries.py
+++ public_law/spiders/ca/doj_glossaries.py
@@ -10,7 +10,6 @@ class DojGlossaries(Spider):
     start_urls = [
         "https://canada.justice.gc.ca/eng/rp-pr/cp-pm/eval/rep-rap/2019/elf-esc/p7.html",
         "https://www.justice.gc.ca/eng/rp-pr/cp-pm/eval/rep-rap/12/lap-paj/p7g.html",
-        "https://www.justice.gc.ca/eng/cj-jp/ad-am/glos.html",
     ]
 
     def parse(self, response: Response, **kwargs: Dict[str, Any]):
