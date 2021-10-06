eb8348f55cf4d0e4723d17613a17f0c5e3c017c6
Robb Shecter <robb@public.law>
2021-10-05 23:38:35 -0600

    test passes

--- public_law/parsers/us/colorado.py
+++ public_law/parsers/us/colorado.py
@@ -1,5 +1,8 @@
 from scrapy.selector.unified import Selector
+from titlecase import titlecase
 
 
-def parse_title(_dom: Selector) -> dict:
-    return {}
+def parse_title(dom: Selector) -> dict:
+    raw_name = dom.xpath("//title_text/text()").get()
+
+    return {"name": titlecase(raw_name)}
