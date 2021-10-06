d907b7b3ebaf8d628f9ebcd430c775c2ef77a93f
Robb Shecter <robb@public.law>
2021-10-06 15:17:38 -0600

    refactor: a little cleanup

--- public_law/parsers/us/colorado/crs.py
+++ public_law/parsers/us/colorado/crs.py
@@ -6,11 +6,13 @@ from public_law.items.crs import Division, Title
 
 def parse_title(dom: Selector) -> Title:
     raw_name = dom.xpath("//title_text/text()").get()
-    raw_division_names = dom.xpath("//t_div/text()").getall()
     raw_number = dom.xpath("//b/text()").get().split(" ")[1]
+
+    raw_division_names = dom.xpath("//t_div/text()").getall()
+    divisions = [Division(name=titlecase(n)) for n in raw_division_names]
 
     return Title(
         name=titlecase(raw_name),
         number=raw_number,
-        divisions=[Division(name=titlecase(n)) for n in raw_division_names],
+        divisions=divisions,
     )
