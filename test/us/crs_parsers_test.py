87896817eaec5e6b7926f1294c750d6c851bd3c5
Robb Shecter <robb@public.law>
2021-10-06 00:47:29 -0600

    passing tests when no Divisions

--- test/us/crs_parsers_test.py
+++ test/us/crs_parsers_test.py
@@ -7,12 +7,19 @@ def fixture(filename: str) -> str:
     return open(f"test/fixtures/{filename}", encoding="utf8").read()
 
 
+# A title with no Divisions.
+TITLE_4 = Selector(text=fixture("crs/title04.txt"))
+
+# A title which does use Divisions.
 TITLE_16 = Selector(text=fixture("crs/title16.txt"))
 
 
 class TestParseTitle:
-    def test_name_of_title(self):
+    def test_name_of_title_1(self):
         assert parse_title(TITLE_16)["name"] == "Criminal Proceedings"
+
+    def test_name_of_title_2(self):
+        assert parse_title(TITLE_4)["name"] == "Uniform Commercial Code"
 
     def test_division_count(self):
         assert len(parse_title(TITLE_16)["divisions"]) == 8
@@ -26,3 +33,8 @@ class TestParseTitle:
         divs = parse_title(TITLE_16)["divisions"]
 
         assert divs[-1] == "Offenders - Registration"
+
+    def test_no_divisions(self):
+        divs = parse_title(TITLE_4)["divisions"]
+
+        assert len(divs) == 0
