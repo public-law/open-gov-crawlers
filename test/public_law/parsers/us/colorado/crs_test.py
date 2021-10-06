eaf6eb4645c43e6693f1b49fc1b0270cff2133ae
Robb Shecter <robb@public.law>
2021-10-06 14:16:56 -0600

    refactor: test DRY

--- test/public_law/parsers/us/colorado/crs_test.py
+++ test/public_law/parsers/us/colorado/crs_test.py
@@ -13,34 +13,37 @@ TITLE_4 = Selector(text=fixture("crs/title04.txt"))
 # A Title which uses Divisions.
 TITLE_16 = Selector(text=fixture("crs/title16.txt"))
 
+PARSED_TITLE_4 = parse_title(TITLE_4)
+PARSED_TITLE_16 = parse_title(TITLE_16)
+
 
 class TestParseTitle:
     def test_name_of_title_1(self):
-        assert parse_title(TITLE_16).name == "Criminal Proceedings"
+        assert PARSED_TITLE_16.name == "Criminal Proceedings"
 
     def test_name_of_title_2(self):
-        assert parse_title(TITLE_4).name == "Uniform Commercial Code"
+        assert PARSED_TITLE_4.name == "Uniform Commercial Code"
 
     def test_title_number_1(self):
-        assert parse_title(TITLE_16).number == "16"
+        assert PARSED_TITLE_16.number == "16"
 
     def test_title_number_2(self):
-        assert parse_title(TITLE_4).number == "4"
+        assert PARSED_TITLE_4.number == "4"
 
     def test_division_count(self):
-        assert len(parse_title(TITLE_16).divisions) == 8
+        assert len(PARSED_TITLE_16.divisions) == 8
 
     def test_first_division_retrieved(self):
-        divs = parse_title(TITLE_16).divisions
+        divs = PARSED_TITLE_16.divisions
 
         assert divs[0] == "Code of Criminal Procedure"
 
     def test_last_division_retrieved(self):
-        divs = parse_title(TITLE_16).divisions
+        divs = PARSED_TITLE_16.divisions
 
         assert divs[-1] == "Offenders - Registration"
 
     def test_no_divisions(self):
-        divs = parse_title(TITLE_4).divisions
+        divs = PARSED_TITLE_4.divisions
 
         assert len(divs) == 0
