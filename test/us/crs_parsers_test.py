8471efca542cf8bdc9fb2ea06ae09f43805c932a
Robb Shecter <robb@public.law>
2021-10-05 22:36:16 -0600

    TITLE_16 fixture

--- test/us/crs_parsers_test.py
+++ test/us/crs_parsers_test.py
@@ -7,6 +7,9 @@ def fixture(filename: str) -> str:
     return open(f"test/fixtures/{filename}", encoding="utf8").read()
 
 
+TITLE_16 = Selector(text=fixture("crs/title16.txt"))
+
+
 class TestParseTitle:
     def test_title_of_title(self):
         assert parse_title(TITLE_16)["name"] == "Criminal Proceedings"
