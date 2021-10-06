add8d7e013cec9db03a97f25593129c50f8e7239
Robb Shecter <robb@public.law>
2021-10-06 16:21:15 -0600

    refactor: extract _parse_divisions()

--- public_law/parsers/us/colorado/crs.py
+++ public_law/parsers/us/colorado/crs.py
@@ -8,15 +8,17 @@ def parse_title(dom: Selector) -> Title:
     raw_name = dom.xpath("//title_text/text()").get()
     raw_number = dom.xpath("//b/text()").get().split(" ")[1]
 
-    raw_division_names = dom.xpath("//t_div/text()").getall()
-    divisions = [Division(name=titlecase(n)) for n in raw_division_names]
-
     url_number = raw_number.rjust(2, "0")
     source_url = f"https://leg.colorado.gov/sites/default/files/images/olls/crs2021-title-{url_number}.pdf"
 
     return Title(
         name=titlecase(raw_name),
         number=raw_number,
-        divisions=divisions,
+        divisions=_parse_divisions(dom),
         source_url=source_url,
     )
+
+
+def _parse_divisions(dom: Selector) -> list[Division]:
+    raw_division_names = dom.xpath("//t_div/text()").getall()
+    return [Division(name=titlecase(n)) for n in raw_division_names]
