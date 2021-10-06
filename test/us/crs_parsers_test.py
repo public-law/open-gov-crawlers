a9ae233a634a1c4a518de355696185a6b13432b9
Robb Shecter <robb@public.law>
2021-10-05 22:32:38 -0600

    first CRS failing test

--- test/us/crs_parsers_test.py
+++ test/us/crs_parsers_test.py
@@ -1,6 +1,12 @@
-# from scrapy.selector import Selector
-# from public_law.parsers.us.colorado import parse_division
+from scrapy.selector import Selector
+
+from public_law.parsers.us.colorado import parse_title
 
 
-# def fixture(filename: str) -> str:
-#     return open(f"test/fixtures/{filename}", encoding="utf8").read()
+def fixture(filename: str) -> str:
+    return open(f"test/fixtures/{filename}", encoding="utf8").read()
+
+
+class TestParseTitle:
+    def test_title_of_title(self):
+        assert parse_title(TITLE_16)["name"] == "Criminal Proceedings"
