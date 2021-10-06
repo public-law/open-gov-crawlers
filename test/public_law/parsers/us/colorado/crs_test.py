643504879849ad9c5a5f3bdce15f68420a09cdf8
Robb Shecter <robb@public.law>
2021-10-06 16:35:45 -0600

    failing test for Division.editors_note

--- test/public_law/parsers/us/colorado/crs_test.py
+++ test/public_law/parsers/us/colorado/crs_test.py
@@ -62,6 +62,14 @@ class TestParseTitle:
             == "https://leg.colorado.gov/sites/default/files/images/olls/crs2021-title-16.pdf"
         )
 
+    def test_title_16_div_1_editors_note(self):
+        div_1 = PARSED_TITLE_16.divisions[0]
+
+        assert (
+            div_1.editors_note
+            == "Articles 1 to 13 of this title (excluding articles 2.5, 2.7, 8.5, 11.3, 11.5, 11.7, 11.8, and 11.9) were numbered as articles 1 to 13 of chapter 39, C.R.S. 1963. The provisions of those articles were repealed and reenacted in 1972, resulting in the addition, relocation, and elimination of sections as well as subject matter. For amendments to those articles prior to 1972, consult the Colorado statutory research explanatory note beginning on page vii in the front of this volume. For a detailed comparison of those articles, see the comparative tables located in the back of the index."
+        )
+
     def test_no_divisions(self):
         divs = PARSED_TITLE_4.divisions
 
