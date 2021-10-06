57a03b5ae499a931e2245cb3e220fb5459a16570
Robb Shecter <robb@public.law>
2021-10-06 16:51:53 -0600

    refactor: test naming

--- test/public_law/parsers/us/colorado/crs_test.py
+++ test/public_law/parsers/us/colorado/crs_test.py
@@ -17,28 +17,28 @@ PARSED_TITLE_16 = parse_title(TITLE_16)
 
 
 class TestParseTitle:
-    def test_name_of_title_16(self):
+    def test_title_16_name(self):
         assert PARSED_TITLE_16.name == "Criminal Proceedings"
 
-    def test_url_of_title_16(self):
+    def test_title_16_url(self):
         assert (
             PARSED_TITLE_16.source_url
             == "https://leg.colorado.gov/sites/default/files/images/olls/crs2021-title-16.pdf"
         )
 
-    def test_url_of_title_4(self):
+    def test_title_4_url(self):
         assert (
             PARSED_TITLE_4.source_url
             == "https://leg.colorado.gov/sites/default/files/images/olls/crs2021-title-04.pdf"
         )
 
-    def test_name_of_title_4(self):
+    def test_title_4_name(self):
         assert PARSED_TITLE_4.name == "Uniform Commercial Code"
 
-    def test_title_number_16(self):
+    def test_title_16_number(self):
         assert PARSED_TITLE_16.number == "16"
 
-    def test_title_number_4(self):
+    def test_title_4_number(self):
         assert PARSED_TITLE_4.number == "4"
 
     def test_division_count(self):
