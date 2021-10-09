828eb81fae53c7452af8d73652ab439a5f989ef0
Robb Shecter <robb@public.law>
2021-10-08 23:22:53 -0600

    just one failiing test

--- public_law/parsers/us/colorado/crs.py
+++ public_law/parsers/us/colorado/crs.py
@@ -5,8 +5,9 @@ from public_law.items.crs import Division, Title
 
 
 def parse_title(dom: Selector) -> Title:
+    print(f"{dom=}")
     raw_name = dom.xpath("//title-text/text()").get()
-    raw_number = dom.xpath("//b/text()").get().split(" ")[1]
+    raw_number = dom.xpath("//title-num/text()").get().split(" ")[1]
 
     url_number = raw_number.rjust(2, "0")
     source_url = f"https://leg.colorado.gov/sites/default/files/images/olls/crs2021-title-{url_number}.pdf"
@@ -20,7 +21,7 @@ def parse_title(dom: Selector) -> Title:
 
 
 def _parse_divisions(dom: Selector, source_url: str) -> list[Division]:
-    raw_division_names = dom.xpath("//t_div/text()").getall()
+    raw_division_names = dom.xpath("//t-div/text()").getall()
 
     return [
         Division(name=titlecase(n), source_url=source_url, articles=[])
