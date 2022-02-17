05dd586f3b340485298a24ec30c79ab71f177af7
Robb Shecter <robb@public.law>
2022-02-17 04:01:48 -0700

    checkpoint

--- test/public_law/parsers/us/colorado/crs_test.py
+++ test/public_law/parsers/us/colorado/crs_test.py
@@ -69,11 +69,17 @@ class TestParseTitle:
         assert len(divs) == 0
 
     def test_correct_number_of_articles_in_division(self):
+        # Title 16 contains eight Divisions.
+        # The first is _Code of Crim. Pro._
+        # This Division contains 22 Articles.
+        expected_article_count = 22
+        #
+
         code_of_crim_pro = PARSED_TITLE_16.divisions[0]
         # Verify we have the correct Division.
         assert code_of_crim_pro.name == "Code of Criminal Procedure"
 
-        assert len(code_of_crim_pro.articles) == 21
+        assert len(code_of_crim_pro.articles) == expected_article_count
 
     # def we_can_get_a_div_editors_note(self):
     #     div_1 = PARSED_TITLE_16.divisions[0]
@@ -82,3 +88,5 @@ class TestParseTitle:
     #         div_1.editors_note
     #         == "Articles 1 to 13 of this title (excluding articles 2.5, 2.7, 8.5, 11.3, 11.5, 11.7, 11.8, and 11.9) were numbered as articles 1 to 13 of chapter 39, C.R.S. 1963. The provisions of those articles were repealed and reenacted in 1972, resulting in the addition, relocation, and elimination of sections as well as subject matter. For amendments to those articles prior to 1972, consult the Colorado statutory research explanatory note beginning on page vii in the front of this volume. For a detailed comparison of those articles, see the comparative tables located in the back of the index."
     #     )
+
+    # def test_article_has_correct_number_of_sections
