dfc43307302cd070b01631ffd375cb72d1bcfe8c
Robb Shecter <robb@public.law>
2022-02-20 01:32:02 -0700

    first two tests

--- test/public_law/text_test.py
+++ test/public_law/text_test.py
@@ -1,3 +1,11 @@
-class TestText:
-    def test_the_test_suite(self):
-        assert True
+from public_law.text import NonemptyString
+
+
+class TestNonemptyString:
+    def test_supports_value_equality(self):
+        ns = NonemptyString("hello")
+        assert ns == "hello"
+
+    def test_supports_lt(self):
+        a = NonemptyString("a")
+        assert a < "b"
