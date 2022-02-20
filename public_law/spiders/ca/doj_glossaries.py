b3a4bc5a80febed251cf0146e188786cb8798dfb
Robb Shecter <robb@public.law>
2022-02-20 04:28:31 -0700

    more canadian glossaries

--- public_law/spiders/ca/doj_glossaries.py
+++ public_law/spiders/ca/doj_glossaries.py
@@ -8,8 +8,14 @@ from public_law.parsers.ca.doj import parse_glossary
 class DojGlossaries(Spider):
     name = "canada_doj_glossaries"
     start_urls = [
-        "https://canada.justice.gc.ca/eng/rp-pr/cp-pm/eval/rep-rap/2019/elf-esc/p7.html",
+        # Manually tested
+        "https://www.justice.gc.ca/eng/rp-pr/cp-pm/eval/rep-rap/2019/elf-esc/p7.html",
         "https://www.justice.gc.ca/eng/rp-pr/cp-pm/eval/rep-rap/12/lap-paj/p7g.html",
+        # Untested
+        "https://laws-lois.justice.gc.ca/eng/glossary/",
+        "https://www.justice.gc.ca/eng/rp-pr/fl-lf/child-enfant/guide/glos.html",
+        "https://www.justice.gc.ca/eng/rp-pr/cp-pm/eval/rep-rap/2019/elf-esc/p7.html",
+        "https://www.justice.gc.ca/eng/rp-pr/fl-lf/spousal-epoux/spag/p18.html",
     ]
 
     def parse(self, response: Response, **kwargs: Dict[str, Any]):
