bf0dfbd8ff2bc93e76a8fd490d9eaf8b891fe4c8
Robb Shecter <robb@public.law>
2022-02-21 03:58:32 -0700

    improved error messages

--- public_law/monitors.py
+++ public_law/monitors.py
@@ -2,18 +2,20 @@ from spidermon import Monitor, MonitorSuite, monitors
 from spidermon.contrib.actions.telegram.notifiers import (
     SendTelegramMessageSpiderFinished,
 )
+from public_law.spiders.ca.doj_glossaries import DojGlossaries
 
 
 @monitors.name("Item count")
 class ItemCountMonitor(Monitor):
-    @monitors.name("Minimum number of items")
-    def test_minimum_number_of_items(self):
-        item_extracted = getattr(self.data.stats, "item_scraped_count", 0)
-        minimum_threshold = 5
+    minimum_threshold = len(DojGlossaries.start_urls)
 
-        self.assertTrue(
-            item_extracted >= minimum_threshold,
-            msg=f"Extracted less than {minimum_threshold} items",
+    @monitors.name(f"Minimum number of items ({minimum_threshold})")
+    def test_minimum_number_of_items(self):
+        item_scraped_count = getattr(self.data.stats, "item_scraped_count", 0)
+
+        self.assertGreaterEqual(
+            item_scraped_count,
+            self.minimum_threshold,
         )
 
 
