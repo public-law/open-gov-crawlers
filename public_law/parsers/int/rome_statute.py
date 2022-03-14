25297a861975739b9e28db8cbe052f8c4e79663e
Robb Shecter <robb@public.law>
2022-03-14 02:14:23 -0600

    passing test for metadata()

--- public_law/parsers/int/rome_statute.py
+++ public_law/parsers/int/rome_statute.py
@@ -3,3 +3,7 @@ from tika import parser
 
 def tika_pdf(url: str) -> dict:
     return parser.from_file(url, xmlContent=True)
+
+
+def metadata(url: str) -> dict:
+    return tika_pdf(url)["metadata"]
--- test/public_law/parsers/int/rome_statute_test.py
+++ test/public_law/parsers/int/rome_statute_test.py
@@ -3,14 +3,23 @@ import urllib
 
 from tika import parser
 
-from public_law.parsers.int.rome_statute import tika_pdf
+from public_law.parsers.int.rome_statute import tika_pdf, metadata
+
+statute_french = "https://www.icc-cpi.int/Publications/Statut-de-Rome.pdf"
 
 
 class TestRomeStatute:
     def test_can_use_the_tika_api(self):
-        french_xml = tika_pdf("https://www.icc-cpi.int/Publications/Statut-de-Rome.pdf")
+        french_xml = tika_pdf(statute_french)
         assert set(french_xml.keys()) == {"metadata", "content", "status"}
 
     def test_raises_error_when_pdf_not_found(self):
         with pytest.raises(urllib.error.HTTPError):
             tika_pdf("https://www.icc-cpi.int/Publications/abcdefg.pdf")
+
+
+class TestMetadata:
+    def test_gets_the_title(self):
+        title = metadata(statute_french)["dc:title"]
+
+        assert title == "Statut de Rome de la Cour pénale internationale"
