37c480159aa8eaf5cfddf69e04f338b819393308
Robb Shecter <robb@public.law>
2022-03-14 01:22:10 -0600

    first failing test

---
+++ public_law/parsers/int/rome_statute.py
@@ -0,0 +1,2 @@
+def pdf_to_xml(url: str):
+    pass
---
+++ test/public_law/parsers/int/rome_statute_test.py
@@ -0,0 +1,12 @@
+from tika import parser
+
+from public_law.parsers.int.rome_statute import pdf_to_xml
+
+
+class TestRomeStatute:
+    def test_can_use_the_tika_api(self):
+        french_xml = pdf_to_xml(
+            "https://www.icc-cpi.int/Publications/Statut-de-Rome.pdf"
+        )
+
+        assert set(french_xml.keys()) == ["metadata", "content", "status"]
