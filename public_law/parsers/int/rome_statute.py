c61c7a63ce848ab9204769fe0ef747120d290158
Robb Shecter <robb@public.law>
2022-03-14 03:39:28 -0600

    add support for arabic and spanish

--- public_law/parsers/int/rome_statute.py
+++ public_law/parsers/int/rome_statute.py
@@ -4,6 +4,8 @@ from tika import parser
 LANGUAGE_MAP = {
     "Rome Statute of the International Criminal Court": "en",
     "Statut de Rome de la Cour pénale internationale": "fr",
+    "نظام روما األسايس للمحكمة اجلنائية ادلويلة": "ar",
+    "Estatuto de Roma de la Corte Penal Internacional": "es",
 }
 
 
