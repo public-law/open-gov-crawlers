24a89879b37cec0cbcf150f310397e95788dbb9a
Robb Shecter <robb@public.law>
2022-03-17 14:34:44 -0600

    feature: start_page_urls()

--- public_law/spiders/int/rome_statute.py
+++ public_law/spiders/int/rome_statute.py
@@ -1,3 +1,4 @@
+import re
 from scrapy import Spider
 from scrapy.http import Response, HtmlResponse
 from typing import Any, Dict
@@ -8,11 +9,26 @@ from public_law.parsers.int.rome_statute import title
 class RomeStatute(Spider):
     name = "rome_statute"
     start_urls = [
-        "https://www.icc-cpi.int/resource-library/Pages/core-legal-texts.aspx",
+        "https://www.icc-cpi.int/resource-library#coreICCtexts",
     ]
 
     def parse(self, response: Response, **kwargs: Dict[str, Any]):
         """Framework callback which begins the parsing."""
         assert isinstance(response, HtmlResponse)
 
-        yield parse_glossary(response)._asdict()
+        pdf_urls = start_page_urls(response)
+
+        # yield parse_glossary(response)._asdict()
+
+
+#
+# Pure helper functions
+#
+
+
+def start_page_urls(response: Response) -> list[str]:
+    anchors = response.css("h2#coreICCtexts + p + div").css("a").getall()[:4]
+    relative_urls = [re.findall(r'"(.+)"', a)[0] for a in anchors]
+    absolute_urls = ["https://www.icc-cpi.int" + url for url in relative_urls]
+
+    return absolute_urls
