29469dd0ec821f128030c9962034cc86ea0cabbf
Robb Shecter <robb@public.law>
2022-04-03 22:20:59 -0600

    refactor: better division among funcs

--- public_law/parsers/us/oregon.py
+++ public_law/parsers/us/oregon.py
@@ -2,7 +2,7 @@ import re
 
 from scrapy.selector.unified import Selector
 from scrapy.http.response import Response
-from typing import Dict, List, Union
+from typing import Dict, List, Tuple, Union
 
 from public_law.items import Rule
 from public_law.text import delete_all
@@ -28,37 +28,40 @@ def parse_division(html: Response) -> List[Rule]:
 
 
 def parse_rule(rule_div: Selector) -> Rule:
-    """A Rule has a number, name, and text content."""
+    """A Rule has a number, name, text, and metadata."""
 
-    number: str = rule_div.css("strong > a::text").get(" ").strip()
-    name: str = rule_div.css("strong::text").get(" ").strip()
-
-    return _parse_rule_content(rule_div, number, name)
-
-
-def _parse_rule_content(rule_div: Selector, number: str, name: str) -> Rule:
-    """Parse the given HTML div for the text, then package it up
-    with the given number and name into a Rule object."""
-
-    raw_paragraphs: List[str] = rule_div.xpath("p")[1:].getall()
-    cleaned_up_paragraphs = [p.strip().replace("\n", "") for p in raw_paragraphs]
-    cleaned_up_paragraphs = [re.sub(r" +", " ", p) for p in cleaned_up_paragraphs]
-    non_empty_paragraphs = list(filter(None, cleaned_up_paragraphs))
-    content_paragraphs = non_empty_paragraphs[1:-1]
-
-    meta_paragraph = non_empty_paragraphs[-1]
-    metadata = meta_sections(meta_paragraph)
+    number = _parse_number(rule_div)
+    name = _parse_name(rule_div)
+    text, metadata = _parse_content(rule_div)
 
     return Rule(
         kind="Rule",
         number=number,
         name=name,
         url=oar_url(f"view.action?ruleNumber={number}"),
-        text="\n".join(content_paragraphs),
+        text=text,
         authority=metadata["authority"],
         implements=metadata["implements"],
         history=metadata["history"],
     )
+
+
+def _parse_content(rule_div: Selector) -> Tuple[str, Dict[str, Union[List[str], str]]]:
+    """Parse the given HTML div for the text string and metadata dict."""
+
+    # Parse the body text
+    raw_paragraphs: List[str] = rule_div.xpath("p")[1:].getall()
+    cleaned_up_paragraphs = [p.strip().replace("\n", "") for p in raw_paragraphs]
+    cleaned_up_paragraphs = [re.sub(r" +", " ", p) for p in cleaned_up_paragraphs]
+    non_empty_paragraphs = list(filter(None, cleaned_up_paragraphs))
+    content_paragraphs = non_empty_paragraphs[1:-1]
+    body_text = "\n".join(content_paragraphs)
+
+    # Parse the metadata
+    meta_paragraph = non_empty_paragraphs[-1]
+    metadata = meta_sections(meta_paragraph)
+
+    return (body_text, metadata)
 
 
 def meta_sections(text: str) -> Dict[str, Union[List[str], str]]:
@@ -118,3 +121,11 @@ URL_PREFIX = f"https://{DOMAIN}/oard/"
 
 def oar_url(relative_fragment: str) -> str:
     return URL_PREFIX + relative_fragment
+
+
+def _parse_number(rule_div: Selector) -> str:
+    return rule_div.css("strong > a::text").get(" ").strip()
+
+
+def _parse_name(rule_div: Selector) -> str:
+    return rule_div.css("strong::text").get(" ").strip()
