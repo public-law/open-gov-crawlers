75bf789eb049e75724a1f69f7b492e9079ed7a2a
Robb Shecter <robb@public.law>
2022-06-28 23:35:14 -0600

    Checking the output's quality

--- public_law/spiders/ca/doj_glossaries.py
+++ public_law/spiders/ca/doj_glossaries.py
@@ -8,15 +8,15 @@ from public_law.parsers.ca.doj import parse_glossary
 class DojGlossaries(Spider):
     name = "canada_doj_glossaries"
     start_urls = [
-        "https://laws-lois.justice.gc.ca/eng/glossary/",
+        # "https://laws-lois.justice.gc.ca/eng/glossary/",
         "https://www.justice.gc.ca/eng/fl-df/parent/mp-fdp/p11.html",
-        "https://www.justice.gc.ca/eng/rp-pr/cp-pm/aud-ver/2011/rc-pmr/01.html",
+        # "https://www.justice.gc.ca/eng/rp-pr/cp-pm/aud-ver/2011/rc-pmr/01.html",       # Crashes.
         "https://www.justice.gc.ca/eng/rp-pr/cp-pm/eval/rep-rap/12/lap-paj/p7g.html",
         "https://www.justice.gc.ca/eng/rp-pr/cp-pm/eval/rep-rap/2019/elf-esc/p7.html",
-        "https://www.justice.gc.ca/eng/rp-pr/fl-lf/child-enfant/guide/glos.html",
+        # "https://www.justice.gc.ca/eng/rp-pr/fl-lf/child-enfant/guide/glos.html",
         "https://www.justice.gc.ca/eng/rp-pr/fl-lf/famil/2003_5/glos.html",
         "https://www.justice.gc.ca/eng/rp-pr/fl-lf/spousal-epoux/calc/aa.html",
-        "https://www.justice.gc.ca/eng/rp-pr/fl-lf/spousal-epoux/spag/p18.html",
+        # "https://www.justice.gc.ca/eng/rp-pr/fl-lf/spousal-epoux/spag/p18.html",
     ]
 
     def parse(self, response: HtmlResponse, **kwargs: Dict[str, Any]):
