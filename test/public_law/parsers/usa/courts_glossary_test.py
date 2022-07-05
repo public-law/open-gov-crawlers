5131e2d46977e7ea549161855d3055b84574f72e
Robb Shecter <robb@public.law>
2022-07-05 00:59:12 -0600

    refactor: tests

--- test/public_law/parsers/usa/courts_glossary_test.py
+++ test/public_law/parsers/usa/courts_glossary_test.py
@@ -19,69 +19,65 @@ def parsed_fixture(filename: str, url: str) -> GlossaryParseResult:
 
 
 @fixture
-def parsed_glossary_uscourts() -> GlossaryParseResult:
+def parsed_glossary() -> GlossaryParseResult:
     return parsed_fixture(
         filename="gov.uscourts-glossary.html", url="https://www.uscourts.gov/glossary"
     )
 
 
-def test_gets_the_name(parsed_glossary_uscourts: GlossaryParseResult):
+def test_gets_the_name(parsed_glossary: GlossaryParseResult):
     assert (
-        parsed_glossary_uscourts.metadata.dcterms_title
+        parsed_glossary.metadata.dcterms_title
         == "Glossary of Legal Terms | United States Courts"
     )
 
 
-@mark.skip(reason="Not implemented yet")
 def test_phrase_does_not_end_with_colon(
-    parsed_glossary_uscourts: GlossaryParseResult,
+    parsed_glossary: GlossaryParseResult,
 ):
-    assert parsed_glossary_uscourts.entries[0].phrase == "Alienated Parent"
+    assert parsed_glossary.entries[0].phrase == "Alienated Parent"
 
 
 @mark.skip(reason="Not implemented yet")
-def test_gets_the_url(parsed_glossary_uscourts: GlossaryParseResult):
+def test_gets_the_url(parsed_glossary: GlossaryParseResult):
     assert (
-        parsed_glossary_uscourts.metadata.dcterms_source
-        == "https://www.justice.gc.ca/eng/rp-pr/cp-pm/eval/rep-rap/12/lap-paj/p7g.html"
+        parsed_glossary.metadata.dcterms_source == "https://www.uscourts.gov/glossary"
     )
 
 
 @mark.skip(reason="Not implemented yet")
-def test_gets_the_author(parsed_glossary_uscourts: GlossaryParseResult):
-    assert parsed_glossary_uscourts.metadata.dcterms_creator == "https://public.law"
+def test_gets_the_author(parsed_glossary: GlossaryParseResult):
+    assert parsed_glossary.metadata.dcterms_creator == "https://public.law"
 
 
 @mark.skip(reason="Not implemented yet")
-def test_gets_the_scrape_date(parsed_glossary_uscourts: GlossaryParseResult):
-    assert parsed_glossary_uscourts.metadata.publiclaw_sourceModified == date(
-        2022, 5, 13
-    )
+def test_gets_the_scrape_date(parsed_glossary: GlossaryParseResult):
+    assert parsed_glossary.metadata.publiclaw_sourceModified == date(2022, 5, 13)
 
 
 @mark.skip(reason="Not implemented yet")
 def test_gets_the_original_modification_date(
-    parsed_glossary_uscourts: GlossaryParseResult,
+    parsed_glossary: GlossaryParseResult,
 ):
-    assert parsed_glossary_uscourts.metadata.dcterms_modified == today()
+    assert parsed_glossary.metadata.dcterms_modified == today()
 
 
 @mark.skip(reason="Not implemented yet")
 def test_gets_proper_number_of_entries(
-    parsed_glossary_uscourts: GlossaryParseResult,
+    parsed_glossary: GlossaryParseResult,
 ):
-    assert len(parsed_glossary_uscourts.entries) == 36
+    assert len(parsed_glossary.entries) == 36
 
 
 @mark.skip(reason="Not implemented yet")
-def test_gets_a_term_case_1(parsed_glossary_uscourts: GlossaryParseResult):
-    entry = parsed_glossary_uscourts.entries[2]
+def test_gets_a_term_case_1(parsed_glossary: GlossaryParseResult):
+    entry = parsed_glossary.entries[2]
     assert entry.phrase == "Adjournment"
     assert entry.definition == "Postponement of a court hearing to another date."
 
 
 @mark.skip(reason="Not implemented yet")
-def test_parses_emphasized_text(parsed_glossary_uscourts: GlossaryParseResult):
+def test_parses_emphasized_text(parsed_glossary: GlossaryParseResult):
     definition_with_em = self.p11_result.entries[0].definition
     expected_definition = (
         "Legal term previously used in the <em>Divorce Act</em> to "
