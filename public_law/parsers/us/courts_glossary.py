040593240c274b8b330a4b4bb6322d60028d36f2
Robb Shecter <robb@public.law>
2022-07-05 02:07:20 -0600

    passing test

--- public_law/parsers/us/courts_glossary.py
+++ public_law/parsers/us/courts_glossary.py
@@ -1,15 +1,16 @@
 # SOURCE_URL = 'https://www.uscourts.gov/glossary'
 # HTML_TITLE = response.xpath('//title/text()').get()
 
+from typing import cast
+from bs4 import BeautifulSoup
 from scrapy.http.response.html import HtmlResponse
 
 from ...text import NonemptyString as NS
-from ...models.glossary import GlossaryParseResult
+from ...models.glossary import GlossaryEntry, GlossaryParseResult
 from ...metadata import Metadata
 
 
 def parse_glossary(html: HtmlResponse) -> GlossaryParseResult:
-
     # pyright: reportUnknownMemberType=false
     return GlossaryParseResult(
         metadata=Metadata(
@@ -21,5 +22,18 @@ def parse_glossary(html: HtmlResponse) -> GlossaryParseResult:
             publiclaw_sourceModified="unknown",
             publiclaw_sourceCreator=NS("United States Courts"),
         ),
-        entries=[],
+        entries=__parse_entries(html),
     )
+
+
+def __parse_entries(html: HtmlResponse) -> list[GlossaryEntry]:
+    soup = BeautifulSoup(cast(str, html.body), "html.parser")
+    raw_entries = zip(soup("dt"), soup("dd"))
+
+    return [
+        GlossaryEntry(
+            phrase=NS(phrase.text),
+            definition=NS(defn.text),
+        )
+        for phrase, defn in raw_entries
+    ]
