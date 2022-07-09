a81882bd12d2702bf463bfdad40da96051b6cb52
Robb Shecter <robb@public.law>
2022-07-08 23:12:05 -0600

    refactor: typing to remove error checking

--- public_law/make_spider_table.py
+++ public_law/make_spider_table.py
@@ -2,10 +2,11 @@
 
 from dataclasses import dataclass
 from types import ModuleType
-from typing import Literal, TypeAlias
-from .text import NonemptyString as String
+from typing import Any, Literal, TypeAlias
 
 import more_itertools
+
+from .text import NonemptyString as String
 
 CODE_REPO_BASE_URL = "https://github.com/public-law/open-gov-crawlers/blob/master"
 DATA_REPO_BASE_URL = "https://github.com/public-law/datasets/blob/master"
@@ -35,9 +36,10 @@ def code_link(name: LinkName, path: String) -> String:
 
 
 @dataclass(frozen=True)
-class SpiderRecord:
+class SpiderRecordWithoutDataLink:
     """
-    One row in the markdown table.
+    One row in the markdown table. It represents a spider whose output hasn't
+    yet been published.
     """
 
     jd_verbose_name: String
@@ -45,7 +47,30 @@ class SpiderRecord:
     parser_path: String
     spider_path: String
     tests_path: String
-    json_path: str  # Can be an empty string if there is no public dataset yet.
+
+    def as_markdown(self) -> str:
+        """
+        Return a markdown-formatted representation of this record.
+
+        E.g.:   "Intergovernmental Rome Statute   [parser] | [spider] | [tests]"
+        """
+        return (
+            f"| {self.jd_verbose_name} | {self.publication_name} "
+            f"| {code_link('parser', self.parser_path)} \\|"
+            f"  {code_link('spider', self.spider_path)} \\|"
+            f"  {code_link('tests', self.tests_path)} | |"
+        )
+
+
+class SpiderRecord(SpiderRecordWithoutDataLink):
+    """
+    One row in the markdown table.
+    """
+    json_path: String  # Can be an empty string if there is no public dataset yet.
+
+    def __init__(self, json_path: String, *args: Any, **kwargs: Any):
+        super().__init__(*args, **kwargs)
+        self.json_path = json_path
 
     def as_markdown(self) -> str:
         """
@@ -61,14 +86,9 @@ class SpiderRecord:
             f"| {self.json_link} |"
         )
 
-
     @property
     def json_link(self) -> str:
-        match self.json_path:
-            case "":
-                return ""
-            case path:
-                return md_link("json", data_url(String(path)))
+        return md_link("json", data_url(String(self.json_path)))
 
 
 @dataclass(frozen=True)
@@ -77,7 +97,7 @@ class MarkdownTable:
     A table of spider records.
     """
 
-    records: tuple[SpiderRecord, ...]
+    records: tuple[SpiderRecord|SpiderRecordWithoutDataLink, ...]
 
     def as_markdown(self) -> str:
         """
@@ -114,14 +134,23 @@ def tests_path(module: ModuleType) -> str:
     return file_path(module).replace(".py", "_test.py")
 
 
-def make_record(module: ModuleType, json_path: str = "") -> SpiderRecord:
+def make_record(module: ModuleType, json_path: String) -> SpiderRecord:
     return SpiderRecord(
+        json_path=json_path,
         jd_verbose_name=String(module.JD_VERBOSE_NAME),
         publication_name=String(module.PUBLICATION_NAME),
         parser_path=String(f"public_law/parsers/{file_path(module)}"),
         spider_path=String(f"public_law/spiders/{file_path(module)}"),
         tests_path=String(f"tests/public_law/parsers/{tests_path(module)}"),
-        json_path=json_path,
+    )
+
+def make_record_without_dataset(module: ModuleType) -> SpiderRecordWithoutDataLink:
+    return SpiderRecordWithoutDataLink(
+        jd_verbose_name=String(module.JD_VERBOSE_NAME),
+        publication_name=String(module.PUBLICATION_NAME),
+        parser_path=String(f"public_law/parsers/{file_path(module)}"),
+        spider_path=String(f"public_law/spiders/{file_path(module)}"),
+        tests_path=String(f"tests/public_law/parsers/{tests_path(module)}"),
     )
 
 
@@ -137,12 +166,12 @@ from .spiders.usa import georgia_ag_opinions, oregon_regs, us_courts_glossary
 
 TABLE = MarkdownTable(
     (
-        make_record(courts_glossary,     "Ireland/courts-glossary.json"),
-        make_record(doj_glossaries,      "Canada/doj-glossaries.json"),
-        make_record(georgia_ag_opinions),
-        make_record(justice_glossary,    "NewZealand/justice-glossary.json"),
-        make_record(oregon_regs),
-        make_record(rome_statute,        "Intergovernmental/RomeStatute/RomeStatute.json"),
-        make_record(us_courts_glossary,  "UnitedStates/us-courts-glossary.json"),
+        make_record(courts_glossary,     String("Ireland/courts-glossary.json")),
+        make_record(doj_glossaries,      String("Canada/doj-glossaries.json")),
+        make_record(justice_glossary,    String("NewZealand/justice-glossary.json")),
+        make_record(rome_statute,        String("Intergovernmental/RomeStatute/RomeStatute.json")),
+        make_record(us_courts_glossary,  String("UnitedStates/us-courts-glossary.json")),
+        make_record_without_dataset(georgia_ag_opinions),
+        make_record_without_dataset(oregon_regs),
     )
 )
