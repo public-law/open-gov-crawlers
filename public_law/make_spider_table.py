dfc9f37f7aaa5924562cc27763265f82d6fb9238
Robb Shecter <robb@public.law>
2022-07-08 23:18:12 -0600

    Refactor parse dont validate (#127)
    * refactor: less code
    
    * refactor
    
    * refactor
    
    * refactor: reduce code
    
    * refactor: formatting and docs
    
    * refactor: clarity
    
    * refactor: typing to remove error checking
    
    * cleanup

--- public_law/make_spider_table.py
+++ public_law/make_spider_table.py
@@ -2,67 +2,93 @@
 
 from dataclasses import dataclass
 from types import ModuleType
+from typing import Any, Literal, TypeAlias
 
 import more_itertools
+
+from .text import NonemptyString as String
 
 CODE_REPO_BASE_URL = "https://github.com/public-law/open-gov-crawlers/blob/master"
 DATA_REPO_BASE_URL = "https://github.com/public-law/datasets/blob/master"
 
 
-def _code_url(path: str) -> str:
-    match path:
-        case "":
-            return ""
-        case _:
-            return f"{CODE_REPO_BASE_URL}/{path}"
+LinkName: TypeAlias = Literal["parser", "spider", "tests", "json"]
 
 
-def _data_url(path: str) -> str:
-    match path:
-        case "":
-            return ""
-        case _:
-            return f"{DATA_REPO_BASE_URL}/{path}"
+def code_url(path: String) -> String:
+    return String(f"{CODE_REPO_BASE_URL}/{path}")
 
 
-def _md_anchor(name: str, url: str) -> str:
-    match url:
-        case "":
-            return ""
-        case _:
-            return f"[{name}]({url})"
+def data_url(path: String) -> String:
+    return String(f"{DATA_REPO_BASE_URL}/{path}")
+
+
+def md_link(name: LinkName, url: String) -> String:
+    return String(f"[{name}]({url})")
+
+
+def code_link(name: LinkName, path: String) -> String:
+    """
+    Return a markdown-formatted link to the code repository.
+    """
+    return md_link(name, code_url(path))
+
 
 
 @dataclass(frozen=True)
-class SpiderRecord:
+class SpiderRecordWithoutDataLink:
     """
-    One row in the markdown table.
+    One row in the markdown table. It represents a spider whose output hasn't
+    yet been published.
     """
 
-    jd_verbose_name: str
-    publication_name: str
-    parser_path: str
-    spider_path: str
-    tests_path: str
-    json_path: str
+    jd_verbose_name: String
+    publication_name: String
+    parser_path: String
+    spider_path: String
+    tests_path: String
 
     def as_markdown(self) -> str:
         """
-        Return a string representation of this record.
+        Return a markdown-formatted representation of this record.
+
+        E.g.:   "Intergovernmental Rome Statute   [parser] | [spider] | [tests]"
         """
-        return f"| {self.jd_verbose_name} | {self.publication_name} | [parser]({self.parser_url()}) \\| [spider]({self.spider_url()}) \\| [tests]({self.tests_url()}) | {self.json_md()} |"
+        return (
+            f"| {self.jd_verbose_name} | {self.publication_name} "
+            f"| {code_link('parser', self.parser_path)} \\|"
+            f"  {code_link('spider', self.spider_path)} \\|"
+            f"  {code_link('tests', self.tests_path)} | |"
+        )
 
-    def parser_url(self) -> str:
-        return _code_url(self.parser_path)
 
-    def spider_url(self) -> str:
-        return _code_url(self.spider_path)
+class SpiderRecord(SpiderRecordWithoutDataLink):
+    """
+    One row in the markdown table.
+    """
+    json_path: String  # Can be an empty string if there is no public dataset yet.
 
-    def tests_url(self) -> str:
-        return _code_url(self.tests_path)
+    def __init__(self, json_path: String, *args: Any, **kwargs: Any):
+        super().__init__(*args, **kwargs)
+        self.json_path = json_path
 
-    def json_md(self) -> str:
-        return _md_anchor("json", _data_url(self.json_path))
+    def as_markdown(self) -> str:
+        """
+        Return a markdown-formatted representation of this record.
+
+        E.g.:   "Intergovernmental Rome Statute   [parser] | [spider] | [tests]   [json]"
+        """
+        return (
+            f"| {self.jd_verbose_name} | {self.publication_name} "
+            f"| {code_link('parser', self.parser_path)} \\|"
+            f"  {code_link('spider', self.spider_path)} \\|"
+            f"  {code_link('tests', self.tests_path)} "
+            f"| {self.json_link} |"
+        )
+
+    @property
+    def json_link(self) -> str:
+        return md_link("json", data_url(String(self.json_path)))
 
 
 @dataclass(frozen=True)
@@ -71,7 +97,7 @@ class MarkdownTable:
     A table of spider records.
     """
 
-    records: tuple[SpiderRecord, ...]
+    records: tuple[SpiderRecord|SpiderRecordWithoutDataLink, ...]
 
     def as_markdown(self) -> str:
         """
@@ -83,7 +109,6 @@ class MarkdownTable:
             "|   |   | Source code | Dataset |\n"
             "| - | - | :---------- | :------ |\n"
         )
-
         body = "\n".join(
             [
                 r.as_markdown()
@@ -108,14 +133,23 @@ def tests_path(module: ModuleType) -> str:
     return file_path(module).replace(".py", "_test.py")
 
 
-def make_record(module: ModuleType, json_path: str = "") -> SpiderRecord:
+def make_record(module: ModuleType, json_path: String) -> SpiderRecord:
     return SpiderRecord(
-        jd_verbose_name=module.JD_VERBOSE_NAME,
-        publication_name=module.PUBLICATION_NAME,
-        parser_path=f"public_law/parsers/{file_path(module)}",
-        spider_path=f"public_law/spiders/{file_path(module)}",
-        tests_path=f"tests/public_law/parsers/{tests_path(module)}",
         json_path=json_path,
+        jd_verbose_name=String(module.JD_VERBOSE_NAME),
+        publication_name=String(module.PUBLICATION_NAME),
+        parser_path=String(f"public_law/parsers/{file_path(module)}"),
+        spider_path=String(f"public_law/spiders/{file_path(module)}"),
+        tests_path=String(f"tests/public_law/parsers/{tests_path(module)}"),
+    )
+
+def make_record_without_dataset(module: ModuleType) -> SpiderRecordWithoutDataLink:
+    return SpiderRecordWithoutDataLink(
+        jd_verbose_name=String(module.JD_VERBOSE_NAME),
+        publication_name=String(module.PUBLICATION_NAME),
+        parser_path=String(f"public_law/parsers/{file_path(module)}"),
+        spider_path=String(f"public_law/spiders/{file_path(module)}"),
+        tests_path=String(f"tests/public_law/parsers/{tests_path(module)}"),
     )
 
 
@@ -131,12 +165,12 @@ from .spiders.usa import georgia_ag_opinions, oregon_regs, us_courts_glossary
 
 TABLE = MarkdownTable(
     (
-        make_record(courts_glossary,     "Ireland/courts-glossary.json"),
-        make_record(doj_glossaries,      "Canada/doj-glossaries.json"),
-        make_record(georgia_ag_opinions),
-        make_record(justice_glossary,    "NewZealand/justice-glossary.json"),
-        make_record(oregon_regs),
-        make_record(rome_statute,        "Intergovernmental/RomeStatute/RomeStatute.json"),
-        make_record(us_courts_glossary,  "UnitedStates/us-courts-glossary.json"),
+        make_record(courts_glossary,     String("Ireland/courts-glossary.json")),
+        make_record(doj_glossaries,      String("Canada/doj-glossaries.json")),
+        make_record(justice_glossary,    String("NewZealand/justice-glossary.json")),
+        make_record(rome_statute,        String("Intergovernmental/RomeStatute/RomeStatute.json")),
+        make_record(us_courts_glossary,  String("UnitedStates/us-courts-glossary.json")),
+        make_record_without_dataset(georgia_ag_opinions),
+        make_record_without_dataset(oregon_regs),
     )
 )
