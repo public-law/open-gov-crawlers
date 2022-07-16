28e33065e184e53af8db9c9c6db38b154f5e6774
Robb Shecter <robb@public.law>
2022-07-16 14:25:27 -0600

    add metadata support for readingEase

--- public_law/metadata.py
+++ public_law/metadata.py
@@ -3,6 +3,7 @@ from datetime import date
 from typing import Any, Literal
 
 from public_law.dates import today
+from public_law.nlp.flesch_reading_ease import Difficulty
 from public_law.text import NonemptyString
 
 
@@ -56,6 +57,8 @@ class Metadata:
     dcterms_source: NonemptyString
     publiclaw_sourceModified: date | Literal["unknown"]
     publiclaw_sourceCreator: NonemptyString
+
+    publiclaw_readingEase: Difficulty | Literal["unknown"] = "unknown"
 
     # This JSON dataset.
     dcterms_creator: str = "https://public.law"
--- tests/public_law/metadata_test.py
+++ tests/public_law/metadata_test.py
@@ -1,63 +1,55 @@
+# pyright: reportUnknownArgumentType=false
+# pyright: reportUnknownMemberType=false
+# pyright: reportUnknownVariableType=false
+# pyright: reportUnknownParameterType=false
+# pyright: reportMissingParameterType=false
+
+from typing import Any
 from public_law.metadata import Metadata
 from public_law.text import NonemptyString as S
 from public_law.dates import today
 
+import pytest
 
-class TestMetadata:
-    def test_creates_expected_dict_1(self):
-        metadata = Metadata(
-            dcterms_source=S("https://a.b.c"),
-            dcterms_title=S("The Title"),
-            dcterms_language="en",
-            dcterms_modified=today(),
-            dcterms_coverage='CAN',
-            publiclaw_sourceModified=today(),
-            publiclaw_sourceCreator=S("Some Canadian Agency"),
-        )
-        generated_dict = metadata.as_dublin_core_dict()
 
-        # fmt: off
-        expected_dict = {
-            "dcterms:creator":          "https://public.law",
-            "dcterms:language":         "en",
-            "dcterms:source":           "https://a.b.c",
-            "dcterms:title":            "The Title",
-            "dcterms:type":             "Dataset",
-            "dcterms:coverage":         "CAN",
-            "dcterms:format":           "application/json",
-            "dcterms:license":          "https://creativecommons.org/licenses/by/4.0/",
-            "dcterms:modified":         today(),
-            "publiclaw:sourceModified": today(),
-            "publiclaw:sourceCreator":  "Some Canadian Agency",
-        }
+@pytest.fixture
+def simple_input() -> Metadata:
+    return Metadata(
+        dcterms_source=S("https://a.b.c"),
+        dcterms_title=S("The Title"),
+        dcterms_language="en",
+        dcterms_modified=today(),
+        dcterms_coverage="CAN",
+        publiclaw_sourceModified=today(),
+        publiclaw_sourceCreator=S("Some Canadian Agency"),
+    )
 
-        assert generated_dict == expected_dict
 
-    def test_creates_expected_dict_2(self):
-        metadata = Metadata(
-            dcterms_source=S("https://a.b.c"),
-            dcterms_title=S("The Title"),
-            dcterms_language="en",
-            dcterms_modified=today(),
-            dcterms_coverage='CAN',
-            publiclaw_sourceModified=today(),
-            publiclaw_sourceCreator=S("Some Canadian Agency"),
-        )
-        generated_dict = dict(metadata)
+@pytest.fixture
+def simple_output() -> dict[str, Any]:
+    return {
+        "dcterms:creator": "https://public.law",
+        "dcterms:language": "en",
+        "dcterms:source": "https://a.b.c",
+        "dcterms:title": "The Title",
+        "dcterms:type": "Dataset",
+        "dcterms:coverage": "CAN",
+        "dcterms:format": "application/json",
+        "dcterms:license": "https://creativecommons.org/licenses/by/4.0/",
+        "dcterms:modified": today(),
+        "publiclaw:readingEase": "unknown",
+        "publiclaw:sourceModified": today(),
+        "publiclaw:sourceCreator": "Some Canadian Agency",
+    }
 
-        # fmt: off
-        expected_dict = {
-            "dcterms:creator":          "https://public.law",
-            "dcterms:language":         "en",
-            "dcterms:source":           "https://a.b.c",
-            "dcterms:title":            "The Title",
-            "dcterms:type":             "Dataset",
-            "dcterms:coverage":         "CAN",
-            "dcterms:format":           "application/json",
-            "dcterms:license":          "https://creativecommons.org/licenses/by/4.0/",
-            "dcterms:modified":         today(),
-            "publiclaw:sourceModified": today(),
-            "publiclaw:sourceCreator":  "Some Canadian Agency",
-        }
 
-        assert generated_dict == expected_dict
+def it_creates_a_dict_directly(simple_input, simple_output):
+    generated_dict = simple_input.as_dublin_core_dict()
+
+    assert generated_dict == simple_output
+
+
+def it_creates_a_dict_via_core_func(simple_input, simple_output):
+    generated_dict = dict(simple_input)
+
+    assert generated_dict == simple_output
