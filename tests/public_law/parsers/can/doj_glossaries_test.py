76efdd378d5aa18d54154db3e35dee6aa805efd0
Robb Shecter <robb@public.law>
2022-07-16 22:27:13 -0600

    refactor: use pytest fixtures

--- tests/public_law/parsers/can/doj_glossaries_test.py
+++ tests/public_law/parsers/can/doj_glossaries_test.py
@@ -1,12 +1,19 @@
-from typing import cast
+# pyright: reportUnknownParameterType=false
+# pyright: reportMissingParameterType=false
+# pyright: reportUnknownMissingType=false
+# pyright: reportUnknownMemberType=false
+# pyright: reportUnknownArgumentType=false
+# pyright: reportUnknownVariableType=false
 
 from datetime import date
-from more_itertools import first, nth
+from typing import cast
 
-from scrapy.http.response.html import HtmlResponse
-from public_law.parsers.can.doj_glossaries import parse_glossary, GlossaryParseResult
+import pytest
+from more_itertools import first, nth
 from public_law.dates import today
 from public_law.models.glossary import GlossaryEntry
+from public_law.parsers.can.doj_glossaries import GlossaryParseResult, parse_glossary
+from scrapy.http.response.html import HtmlResponse
 
 
 def parsed_fixture(filename: str, url: str) -> GlossaryParseResult:
@@ -20,89 +27,96 @@ def parsed_fixture(filename: str, url: str) -> GlossaryParseResult:
     return parse_glossary(html)
 
 
-def parsed_glossary() -> GlossaryParseResult:
+@pytest.fixture
+def p7g() -> GlossaryParseResult:
     return parsed_fixture(
         "p7g.html",
         "https://www.justice.gc.ca/eng/rp-pr/cp-pm/eval/rep-rap/12/lap-paj/p7g.html",
     )
 
 
-def parsed_glossary_p11() -> GlossaryParseResult:
+@pytest.fixture
+def p11() -> GlossaryParseResult:
     return parsed_fixture(
         "p11.html", "https://www.justice.gc.ca/eng/fl-df/parent/mp-fdp/p11.html"
     )
 
 
-def parsed_glossary_glos() -> GlossaryParseResult:
+@pytest.fixture
+def glos() -> GlossaryParseResult:
     return parsed_fixture(
         "glos.html", "https://www.justice.gc.ca/eng/rp-pr/fl-lf/famil/2003_5/glos.html"
     )
 
 
-def parsed_glossary_index() -> GlossaryParseResult:
+@pytest.fixture
+def index() -> GlossaryParseResult:
     return parsed_fixture("index.html", "https://laws-lois.justice.gc.ca/eng/glossary/")
 
 
-class TestParseGlossary:
-    # pyright: reportUninitializedInstanceVariable=false
-    def setup(self):
-        self.result = parsed_glossary()
-        self.p11_result = parsed_glossary_p11()
+def test_the_name(p7g):
+    assert (
+        p7g.metadata.dcterms_title
+        == "GLOSSARY OF LEGAL TERMS - Legal Aid Program Evaluation"
+    )
 
-    def test_gets_the_name(self):
-        assert (
-            self.result.metadata.dcterms_title
-            == "GLOSSARY OF LEGAL TERMS - Legal Aid Program Evaluation"
-        )
 
-    def test_gets_the_name_when_it_contains_an_anchor(self):
-        assert (
-            parsed_glossary_glos().metadata.dcterms_title
-            == "GLOSSARY - Managing Contact Difficulties: A Child-Centred Approach (2003-FCY-5E)"  # "Managing Contact Difficulties: A Child-Centred Approach; GLOSSARY"
-        )
+def test_the_name_when_it_contains_an_anchor(glos):
+    assert (
+        glos.metadata.dcterms_title
+        == "GLOSSARY - Managing Contact Difficulties: A Child-Centred Approach (2003-FCY-5E)"  # "Managing Contact Difficulties: A Child-Centred Approach; GLOSSARY"
+    )
 
-    def test_phrase_does_not_end_with_colon(self):
-        assert first(parsed_glossary_glos().entries).phrase == "Alienated Parent"
 
-    def test_gets_the_name_when_there_is_just_an_h1(self):
-        assert (
-            parsed_glossary_index().metadata.dcterms_title == "Glossary"
-        )  # Unfortunately.
+def test_phrase_does_not_end_with_colon(glos):
+    assert first(glos.entries).phrase == "Alienated Parent"
 
-    def test_gets_the_url(self):
-        assert (
-            self.result.metadata.dcterms_source
-            == "https://www.justice.gc.ca/eng/rp-pr/cp-pm/eval/rep-rap/12/lap-paj/p7g.html"
-        )
 
-    def test_gets_the_author(self):
-        assert self.result.metadata.dcterms_creator == "https://public.law"
+def test_the_name_when_there_is_just_an_h1(index):
+    assert index.metadata.dcterms_title == "Glossary"  # Unfortunately.
 
-    def test_gets_the_scrape_date(self):
-        assert self.result.metadata.publiclaw_sourceModified == date(2022, 5, 13)
 
-    def test_gets_the_original_modification_date(self):
-        assert self.result.metadata.dcterms_modified == today()
+def test_the_url(p7g):
+    assert (
+        p7g.metadata.dcterms_source
+        == "https://www.justice.gc.ca/eng/rp-pr/cp-pm/eval/rep-rap/12/lap-paj/p7g.html"
+    )
 
-    def test_gets_proper_number_of_entries(self):
-        assert len(tuple(self.result.entries)) == 36
 
-    def test_gets_a_term_case_1(self):
-        entry = cast(GlossaryEntry, nth(self.result.entries, 2))
+def test_the_author(p7g):
+    assert p7g.metadata.dcterms_creator == "https://public.law"
 
-        assert entry.phrase == "Adjournment"
-        assert entry.definition == "Postponement of a court hearing to another date."
 
-    def test_parses_emphasized_text(self):
-        definition_with_em = first(self.p11_result.entries).definition
-        expected_definition = (
-            "Legal term previously used in the <em>Divorce Act</em> to "
-            "refer to the time a parent or other person spends with a "
-            "child, usually not the parent with whom the child primarily "
-            "lives."
-        )
+def test_the_scrape_date(p7g):
+    assert p7g.metadata.publiclaw_sourceModified == date(2022, 5, 13)
 
-        assert definition_with_em == expected_definition
 
-    def test_reading_ease(self):
-        assert parsed_glossary().metadata.publiclaw_readingEase == "Difficult"
+def test_the_original_modification_date(p7g):
+    assert p7g.metadata.dcterms_modified == today()
+
+
+def test_proper_number_of_entries(p7g):
+    assert len(tuple(p7g.entries)) == 36
+
+
+def test_a_term_case_1(p7g):
+    entry = cast(GlossaryEntry, nth(p7g.entries, 2))
+
+    assert entry.phrase == "Adjournment"
+    assert entry.definition == "Postponement of a court hearing to another date."
+
+
+def test_parses_emphasized_text(p11):
+    definition_with_em = first(p11.entries).definition
+    expected_definition = (
+        "Legal term previously used in the <em>Divorce Act</em> to "
+        "refer to the time a parent or other person spends with a "
+        "child, usually not the parent with whom the child primarily "
+        "lives."
+    )
+
+    assert definition_with_em == expected_definition
+
+
+def test_reading_ease(p7g):
+    assert p7g.metadata.publiclaw_readingEase == "Difficult"
