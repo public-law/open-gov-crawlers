2f234658ebb1c5c78198e9ceb1cb62c265ec1217
Robb Shecter <robb@public.law>
2022-08-11 21:39:35 -0600

    formatting

--- tests/public_law/parsers/aus/ip_glossary_test.py
+++ tests/public_law/parsers/aus/ip_glossary_test.py
@@ -1,4 +1,5 @@
 from datetime import date
+
 from more_itertools import first, last
 from public_law.dates import today
 from public_law.metadata import Subject
@@ -29,6 +30,7 @@ PARSED_GLOSSARY_FIXTURE = parsed_fixture(
 # Tests for metadata.
 #
 
+
 def test_the_name():
     assert PARSED_GLOSSARY_FIXTURE.metadata.dcterms_title == "IP Glossary"
 
@@ -48,13 +50,15 @@ def test_coverage():
     assert PARSED_GLOSSARY_FIXTURE.metadata.dcterms_coverage == "AUS"
 
 
-def test_source_modified_date():
-    # <span class="date-display-single" property="dc:date" datatype="xsd:dateTime" content="2021-03-26T00:00:00+11:00">26 March 2021</span>
-    assert PARSED_GLOSSARY_FIXTURE.metadata.publiclaw_sourceModified == date(2021, 3, 26)
-
-
 def test_scrape_date():
     assert PARSED_GLOSSARY_FIXTURE.metadata.dcterms_modified == today()
+
+
+def test_source_modified_date():
+    # <span class="date-display-single" property="dc:date" datatype="xsd:dateTime" content="2021-03-26T00:00:00+11:00">26 March 2021</span>
+    assert PARSED_GLOSSARY_FIXTURE.metadata.publiclaw_sourceModified == date(
+        2021, 3, 26
+    )
 
 
 #
