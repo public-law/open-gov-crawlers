69fa0695c15aeda1cc55ecbe60ef88b2083ef758
Robb Shecter <robb@public.law>
2022-08-18 21:32:29 -0600

    refactor: terser syntax

--- tests/public_law/parsers/usa/uscis_glossary_test.py
+++ tests/public_law/parsers/usa/uscis_glossary_test.py
@@ -34,54 +34,54 @@ def parsed_glossary() -> GlossaryParseResult:
         url="https://www.uscis.gov/tools/glossary",
     )
 
-@mark.skip()
+@mark.skip
 def test_gets_the_name(parsed_glossary: GlossaryParseResult):
     assert parsed_glossary.metadata.dcterms_title == "USCIS Glossary"
 
 
-@mark.skip()
+@mark.skip
 def test_gets_the_url(parsed_glossary: GlossaryParseResult):
     assert parsed_glossary.metadata.dcterms_source == "https://www.uscis.gov/tools/glossary"
 
 
-@mark.skip()
+@mark.skip
 def test_gets_the_author(parsed_glossary: GlossaryParseResult):
     assert parsed_glossary.metadata.dcterms_creator == "https://public.law"
 
 
-@mark.skip()
+@mark.skip
 def test_gets_coverage(parsed_glossary: GlossaryParseResult):
     assert parsed_glossary.metadata.dcterms_coverage == "USA"
 
 
-@mark.skip()
+@mark.skip
 def test_gets_the_source_modified_date(parsed_glossary: GlossaryParseResult):
     assert parsed_glossary.metadata.publiclaw_sourceModified == "unknown"
 
 
-@mark.skip()
+@mark.skip
 def test_gets_the_scrape_date(parsed_glossary: GlossaryParseResult):
     assert parsed_glossary.metadata.dcterms_modified == today()
 
 
-@mark.skip()
+@mark.skip
 def test_phrase(parsed_glossary: GlossaryParseResult):
     assert first(parsed_glossary.entries).phrase == "Alien Registration Number"
 
 
-@mark.skip()
+@mark.skip
 def test_definition(parsed_glossary: GlossaryParseResult):
     assert (
         first(parsed_glossary.entries).definition == "A unique seven-, eight- or nine-digit number assigned to a noncitizen by the Department of Homeland Security. Also see USCIS Number."
     )
 
 
-@mark.skip()
+@mark.skip
 def test_gets_proper_number_of_entries(parsed_glossary: GlossaryParseResult):
     assert len(tuple(parsed_glossary.entries)) == 43
 
 
-@mark.skip()
+@mark.skip
 def test_gets_the_last_entry(parsed_glossary: GlossaryParseResult):
     last_entry = last(parsed_glossary.entries)
 
@@ -93,7 +93,7 @@ def test_gets_the_last_entry(parsed_glossary: GlossaryParseResult):
     )
 
 
-@mark.skip()
+@mark.skip
 def test_subjects(parsed_glossary: GlossaryParseResult):
     assert parsed_glossary.metadata.dcterms_subject == (
         Subject(
