459d3deffb3b600dfe070c08738e81f44b4d9c87
Robb Shecter <robb@public.law>
2022-08-22 18:34:14 -0600

    all tests pass

--- public_law/parsers/can/doj_glossaries.py
+++ public_law/parsers/can/doj_glossaries.py
@@ -186,4 +186,4 @@ def first_match(node: SelectorLike, css: str, expected: str) -> str:
 
 
 def fix_title_case(text: str) -> str:
-    return text.replace('GLOSSARY OF LEGAL TERMS', 'Glossary of Legal Terms').replace('GLOSSARY OF TERMS', 'Glossary of Terms')
+    return text.replace('GLOSSARY OF LEGAL TERMS', 'Glossary of Legal Terms').replace('GLOSSARY OF TERMS', 'Glossary of Terms').replace('GLOSSARY', 'Glossary')
