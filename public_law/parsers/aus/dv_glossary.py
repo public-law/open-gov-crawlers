361de47fe980f3e929cb4d0abaef6afdeb55ca69
Robb Shecter <robb@public.law>
2022-08-22 23:38:06 -0600

    good failures

---
+++ public_law/parsers/aus/dv_glossary.py
@@ -0,0 +1,55 @@
+from typing import Any, Iterable
+
+from scrapy.http.response.html import HtmlResponse
+
+from ...metadata import Metadata, Subject
+from ...models.glossary import GlossaryEntry, GlossaryParseResult, reading_ease
+from ...text import URL, LoCSubject, NonemptyString as String
+from ...text import Sentence, ensure_ends_with_period, make_soup, normalize_nonempty
+
+
+def parse_glossary(html: HtmlResponse) -> GlossaryParseResult:
+    parsed_entries = tuple(__parse_entries(html))
+
+    return GlossaryParseResult(
+        metadata=Metadata(
+            dcterms_title=String("Glossary"),
+            dcterms_language="en",
+            dcterms_coverage="NZL",
+            # Info about original source
+            dcterms_source=String("https://www.justice.govt.nz/about/glossary/"),
+            publiclaw_sourceModified="unknown",
+            publiclaw_sourceCreator=String("New Zealand Ministry of Justice"),
+            dcterms_subject=(
+                Subject(
+                    uri=LoCSubject("sh85071120"),  # type: ignore
+                    rdfs_label=String("Justice, Administration of"),
+                ),
+                Subject(
+                    uri=URL("https://www.wikidata.org/wiki/Q16514399"),
+                    rdfs_label=String("Administration of justice"),
+                ),
+            ),
+        ),
+        entries=parsed_entries,
+    )
+
+
+def __parse_entries(html: HtmlResponse) -> Iterable[GlossaryEntry]:
+    """TODO: Refactor into a parent class"""
+
+    for phrase, defn in __raw_entries(html):
+        yield GlossaryEntry(
+            phrase=normalize_nonempty(phrase.text),
+            definition=Sentence(normalize_nonempty(ensure_ends_with_period(defn.text))),
+        )
+
+
+def __raw_entries(html: HtmlResponse) -> Iterable[tuple[Any, Any]]:
+    """
+    The core of this parser.
+
+    TODO: Refactor all the glossary parsers to need only this function.
+    """
+    soup = make_soup(html)
+    return ((phrase, phrase.parent.next_sibling) for phrase in soup.find_all("strong"))
---
+++ tests/public_law/parsers/aus/dv_glossary_test.py
@@ -0,0 +1,103 @@
+# pyright: reportSelfClsParameterName=false
+from devtools import debug  # type: ignore
+from more_itertools import first, last, nth
+from scrapy.http.response.html import HtmlResponse
+
+from public_law.dates import today
+from public_law.metadata import Subject
+from public_law.models.glossary import GlossaryParseResult
+from public_law.text import URL, NonemptyString
+
+# The System Under Test
+from public_law.parsers.aus.dv_glossary import parse_glossary
+
+
+def parsed_fixture(filename: str, jd_slug: str, url: URL) -> GlossaryParseResult:
+    """
+    Create a GlossaryParseResult using the three required parameters.
+    """
+    with open(f"tests/fixtures/{jd_slug}/{filename}", encoding="utf8") as f:
+        html = HtmlResponse(
+            url=url,
+            body=f.read(),
+            encoding="UTF-8",
+        )
+
+    return parse_glossary(html)
+
+
+GLOSSARY_URL = URL(
+    "https://www.aihw.gov.au/reports-data/behaviours-risk-factors/domestic-violence/glossary"
+)
+
+GLOSSARY = parsed_fixture(
+    filename="dv-glossary.html",
+    jd_slug="aus",
+    url=GLOSSARY_URL
+    )
+
+METADATA = GLOSSARY.metadata
+
+
+class TestTheMetadata:
+    def test_the_name(_):
+        assert METADATA.dcterms_title == 'Family, domestic and sexual violence glossary'
+
+    def test_the_url(_):
+        assert METADATA.dcterms_source == GLOSSARY_URL
+
+    def test_the_author(_):
+        assert METADATA.dcterms_creator == "https://public.law"
+
+    def test_coverage(_):
+        assert METADATA.dcterms_coverage == "AUS"
+
+    def test_creator(_):
+        assert METADATA.publiclaw_sourceCreator == "Australia Institute of Health and Welfare"
+
+    def test_the_source_modified_date(_):
+        assert METADATA.publiclaw_sourceModified == "unknown"
+
+    def test_the_scrape_date(_):
+        assert METADATA.dcterms_modified == today()
+
+    def test_subjects(_):
+        assert METADATA.dcterms_subject == (
+            Subject(
+                uri=URL("http://id.loc.gov/authorities/subjects/sh85075807"),
+                rdfs_label=NonemptyString("Legislative bodies"),
+            ),
+            Subject(
+                uri=URL("https://www.wikidata.org/wiki/Q35749"),
+                rdfs_label=NonemptyString("Parliament"),
+            ),
+        )
+
+
+class TestTheEntries:
+    def test_phrase(_):
+        assert first(GLOSSARY.entries).phrase == "arranged marriage"
+
+    def test_definition(_):
+        assert first(GLOSSARY.entries).definition == (
+            "Distinct from forced marriage, an arranged marriage is organised "
+            "by the families of both spouses, but consent is still present, "
+            "and the spouses have the right to accept or reject the marriage arrangement."
+        )
+
+    def test_proper_number_of_entries(_):
+        assert len(tuple(GLOSSARY.entries)) == 56
+
+    def test_the_last_entry_phrase(_):
+        last_entry = last(GLOSSARY.entries)
+
+        assert last_entry.phrase == "vulnerable groups"
+
+    def test_the_last_entry_definition(_):
+        last_entry = last(GLOSSARY.entries)
+
+        assert last_entry.definition == (
+            "Population groups that are more likely to experience (or to have experienced) "
+            "family, domestic and sexual violence, or that face additional barriers in "
+            "coping with and recovering from family, domestic and sexual violence."
+        )
