0ea7158471562c4637697ab428057cde853e39cc
Robb Shecter <robb@public.law>
2023-06-21 18:50:27 -0600

    fix: type errors

--- public_law/parsers/int/rome_statute.py
+++ public_law/parsers/int/rome_statute.py
@@ -7,6 +7,7 @@ from pydantic import BaseModel, conint, constr
 from tika import parser
 from titlecase import titlecase
 
+from public_law.metadata import Metadata
 from public_law.text import NonemptyString as S, normalize_whitespace
 
 LANGUAGE_MAP = {
@@ -30,8 +31,8 @@ class Part(FrozenModel):
     """Represents a 'Part' in the text of the Rome Statute.
     It's basically like a chapter. A Part has many Articles."""
 
-    number: conint(ge=1, le=13) # type: ignore
-    name: constr(regex=r"^[ a-zA-Z,]+$") # type: ignore
+    number: conint(ge=1, le=13)
+    name: constr(regex=r"^[ a-zA-Z,]+$")
 
 
 class Article(FrozenModel):
@@ -39,14 +40,14 @@ class Article(FrozenModel):
     section of the statute. An Article belongs to one Part."""
 
     number: str  # Is string because of numbers like "8 bis".
-    part_number: conint(ge=1, le=13)  # type: ignore
+    part_number: conint(ge=1, le=13)  # pyright: reportGeneralTypeIssues=false
     name: constr(
         regex=r"^[ a-zA-Z0-9,:\-\(\)]*$"
-    )  # type: ignore
+    )  # pyright: reportGeneralTypeIssues=false
     text: str
 
-    def name_str(self) -> str:
-        return cast(str, self.name) # type: ignore
+    def name(self) -> str:
+        return cast(str, self.name)
 
     def part_number(self) -> int:
         return cast(int, self.part_number)
@@ -56,7 +57,7 @@ class Footnote(FrozenModel):
     """Represents a footnote in the document. Each one belongs
     to an Article. There are 10 in the English version."""
 
-    number: conint(ge=1, le=10) # type: ignore
+    number: conint(ge=1, le=10)
     article_number: S
     text: S
 
@@ -134,6 +135,17 @@ def footnotes() -> list[Footnote]:
             ),
         ),
     ]
+
+
+def new_metadata(pdf_url: str) -> Metadata:
+    # pdf_data = metadata(pdf_url)
+
+    return Metadata(
+        dc_identifier=S(JSON_OUTPUT_URL_EN),
+        dc_source=S(pdf_url),
+        dc_title=S(title(pdf_url)),
+        dc_language=S(language(pdf_url)),
+    )
 
 
 @cache
@@ -265,12 +277,12 @@ def _article_number(number_raw: str, current_article_num: int) -> str:
 
 def _remove_annotations(article: Article, number: str) -> Article:
     """Remove annotations from text if they exist."""
-    name = article.name_str()
+    name = article.name
     text = article.text
     annotation = article.number.replace(str(number), "")
     if annotation:
         name_text = article.text.split("\n", 1)
-        if len(name_text) > 1 and len(article.name_str()) == 0:
+        if len(name_text) > 1 and len(article.name) == 0:
             name = name_text[0].strip()
             text = name_text[1].strip()
         annotations = [int(x) for x in annotation.split()]
