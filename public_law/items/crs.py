21e4ddbc2f69d69c8a5cbef1e2c20d3d81ab5084
Robb Shecter <robb@public.law>
2023-06-22 04:06:30 -0600

    updated the item classes

--- public_law/items/crs.py
+++ public_law/items/crs.py
@@ -4,20 +4,30 @@ from dataclasses import dataclass
 # Items for the Colorado Revised Statutes.
 #
 
+@dataclass(frozen=True)
+class Section:
+    """A CRS Section."""
+    name: str
+    number: str
+    source_url: str
+    text: str
+
 
 @dataclass(frozen=True)
 class Article:
     """A CRS Article."""
-
     name: str
     number: str
     source_url: str
+    sections: list[Section]
 
 
 @dataclass(frozen=True)
 class Division:
-    """A CRS Division."""
+    """A CRS Division.
 
+    Some titles have Divisions, others don't.
+    """
     name: str
     source_url: str
     articles: list[Article]
@@ -26,8 +36,8 @@ class Division:
 @dataclass(frozen=True)
 class Title:
     """A CRS Title."""
-
     name: str
     number: str
     source_url: str
     divisions: list[Division]
+    articles: list[Article]
