3986629ea5e9644ea4acfdef13f49dcdcc6180b0
Robb Shecter <robb@public.law>
2023-06-22 21:55:59 -0600

    refactor: cleanup

--- tests/public_law/parsers/usa/crs_divisions_test.py
+++ tests/public_law/parsers/usa/crs_divisions_test.py
@@ -46,3 +46,14 @@ class TestParseDivisions:
             last_division.source_url
             == "https://leg.colorado.gov/sites/default/files/images/olls/crs2022-title-16.pdf"
         )
+
+
+    # def we_can_get_a_div_editors_note(self):
+    #     div_1 = PARSED_TITLE_16.children[0]
+
+    #     assert (
+    #         div_1.editors_note
+    #         == "Articles 1 to 13 of this title (excluding articles 2.5, 2.7, 8.5, 11.3, 11.5, 11.7, 11.8, and 11.9) were numbered as articles 1 to 13 of chapter 39, C.R.S. 1963. The provisions of those articles were repealed and reenacted in 1972, resulting in the addition, relocation, and elimination of sections as well as subject matter. For amendments to those articles prior to 1972, consult the Colorado statutory research explanatory note beginning on page vii in the front of this volume. For a detailed comparison of those articles, see the comparative tables located in the back of the index."
+    #     )
+
+    # def test_article_has_correct_number_of_sections
--- tests/public_law/parsers/usa/crs_sections_test.py
+++ tests/public_law/parsers/usa/crs_sections_test.py
@@ -21,7 +21,7 @@ PARSED_TITLE_16 = parse_title(TITLE_16)
 
 
 class TestParseSection:
-    def test_article_has_sections_1(self):
+    def test_article_has_correct_number_of_sections_1(self):
         code_of_crim_pro   = cast(Division, PARSED_TITLE_16.children[0])
         general_provisions = code_of_crim_pro.articles[0]
 
@@ -42,14 +42,3 @@ class TestParseSection:
         first_section      = general_provisions.sections[0]
 
         assert first_section.name == "Short Title"
-
-
-    # def we_can_get_a_div_editors_note(self):
-    #     div_1 = PARSED_TITLE_16.children[0]
-
-    #     assert (
-    #         div_1.editors_note
-    #         == "Articles 1 to 13 of this title (excluding articles 2.5, 2.7, 8.5, 11.3, 11.5, 11.7, 11.8, and 11.9) were numbered as articles 1 to 13 of chapter 39, C.R.S. 1963. The provisions of those articles were repealed and reenacted in 1972, resulting in the addition, relocation, and elimination of sections as well as subject matter. For amendments to those articles prior to 1972, consult the Colorado statutory research explanatory note beginning on page vii in the front of this volume. For a detailed comparison of those articles, see the comparative tables located in the back of the index."
-    #     )
-
-    # def test_article_has_correct_number_of_sections
