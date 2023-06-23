1ed1b3fea440adfa3afa01331cdcc3c192ed0bd7
Robb Shecter <robb@public.law>
2023-06-23 01:24:08 -0600

    refactor

--- tests/public_law/parsers/usa/crs_sections_test.py
+++ tests/public_law/parsers/usa/crs_sections_test.py
@@ -7,6 +7,7 @@ from public_law.parsers.usa.colorado.crs import parse_sections
 # A Title which uses Divisions.
 TITLE_16 = Selector(text = fixture('usa', 'crs', "title16.xml"))
 TITLE_16_SECTIONS = parse_sections(TITLE_16)
+ARTICLE_1_SECTIONS = [s for s in TITLE_16_SECTIONS if s.article_number == "1"]
 
 
 class TestParseSection:
@@ -14,21 +15,14 @@ class TestParseSection:
         """The parsing strategy is JSON lines. And so there isn't a
         direct API for this.
         """
-
-        art_1_sections = [s for s in TITLE_16_SECTIONS if s.article_number == "1"]
-
-        assert len(art_1_sections) == 10
+        assert len(ARTICLE_1_SECTIONS) == 10
 
 
     def test_section_number_1(self):
-        art_1_sections = [s for s in TITLE_16_SECTIONS if s.article_number == "1"]
-        first_section  = art_1_sections[0]
-
+        first_section  = ARTICLE_1_SECTIONS[0]
         assert first_section.number == "16-1-101"
 
 
     def test_section_name_1(self):
-        art_1_sections = [s for s in TITLE_16_SECTIONS if s.article_number == "1"]
-        first_section  = art_1_sections[0]
-
+        first_section  = ARTICLE_1_SECTIONS[0]
         assert first_section.name == "Short Title"
