6485f093b15b0e97b9ad044122aebb4d3f482e16
Robb Shecter <robb@public.law>
2023-06-24 01:34:30 -0600

    Read from a directory

--- public_law/spiders/usa/colorado_crs.py
+++ public_law/spiders/usa/colorado_crs.py
@@ -1,17 +1,26 @@
+import os
+from pathlib import Path
+
 from scrapy import Spider
+from scrapy.http.request import Request
 from scrapy.http.response.html import HtmlResponse
 from typing import Any
 
-# from ...parsers.usa.uscis_glossary import parse_glossary
-
 
 class ColoradoCRS(Spider):
-    name = "usa_colorado_crs"
+    """Spider for the Colorado CRS XML files.
 
-    start_urls = ["File://tmp/sources/CRSDATA20220915/TITLES"]
+    Reads the sources from a local directory instead of the web.
+    """
+    name = "usa_colorado_crs"
+    XML_DIR  = f"{os.getcwd()}/tmp/sources/CRSDATA20220915/TITLES"
+
+    def start_requests(self):
+        for path in sorted(Path(self.XML_DIR).glob("*.xml")):
+            yield Request(url=f"file://{path}", callback=self.parse)
+
 
     def parse(self, response: HtmlResponse, **_: dict[str, Any]):
         """Framework callback which begins the parsing.
         """
-
-        yield response.body
+        print(f"response.url: {response.url}")
