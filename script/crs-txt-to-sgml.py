10b7b87931dcc7aa7fb08ee81e1c193d6ff87680
Robb Shecter <robb@public.law>
2023-07-21 23:06:57 -0600

    more fixes

--- script/crs-txt-to-sgml.py
+++ script/crs-txt-to-sgml.py
@@ -55,6 +55,7 @@ def fix_unencoded_text(line: str) -> str:
         .replace("M&S", "M&amp;S")
         .replace("&A ", "&amp;A ")
         .replace("&ampl ", "&amp; ")
+        .replace("CF&I", 'CF&amp;I')
         .replace(chr(21), "")
         .replace(chr(12), "")
     )
