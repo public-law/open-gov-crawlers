e281f07947150a461b0367b9554ef6a4383804ce
Robb Shecter <robb@public.law>
2024-12-20 18:07:56 -0700

    hide more type errors

--- public_law/spiders/aus/ip_glossary.py
+++ public_law/spiders/aus/ip_glossary.py
@@ -15,7 +15,7 @@ class IPGlossary(Spider):
         "https://raw.githubusercontent.com/public-law/datasets/master/Australia/ip-glossary.html"
     ]
 
-    def parse(self, response: HtmlResponse, **_: dict[str, Any]): # type: ignore[override]
+    def parse(self, response: HtmlResponse, **_: dict[str, Any]):
         """Framework callback which begins the parsing.
 
         @url https://raw.githubusercontent.com/public-law/datasets/master/Australia/ip-glossary.html
