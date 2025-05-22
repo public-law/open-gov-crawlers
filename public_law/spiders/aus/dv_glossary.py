3ea03fd18cd86a8cddd8cc774957adf5ee17bead
Robb Shecter <robb@public.law>
2025-05-21 21:14:39 -0600

    refactor

--- public_law/spiders/aus/dv_glossary.py
+++ public_law/spiders/aus/dv_glossary.py
@@ -1,27 +1,22 @@
 from typing import Any
 
-from scrapy import Spider
 from scrapy.http.response.html import HtmlResponse
 
 from ...parsers.aus.dv_glossary import parse_glossary
+from ...models.glossary import GlossaryParseResult
+from ..base import BaseGlossarySpider
 
 JD_VERBOSE_NAME = "Australia"
 PUBLICATION_NAME = "Family, domestic and sexual violence glossary"
 
 
-class DVGlossary(Spider):
+class DVGlossary(BaseGlossarySpider):
     name = "aus_dv_glossary"
 
     start_urls = [
         "https://www.aihw.gov.au/reports-data/behaviours-risk-factors/domestic-violence/glossary"
     ]
 
-    def parse(self, response: HtmlResponse, **_: dict[str, Any]):
-        """Framework callback which begins the parsing.
-
-        @url https://www.aihw.gov.au/reports-data/behaviours-risk-factors/domestic-violence/glossary
-        @returns items 1 1
-        @returns requests 0 0
-        @scrapes metadata entries
-        """
-        yield parse_glossary(response).asdict()
+    def parse_glossary(self, response: HtmlResponse) -> GlossaryParseResult:
+        """Framework callback which begins the parsing."""
+        return parse_glossary(response)
--- public_law/spiders/nzl/justice_glossary.py
+++ public_law/spiders/nzl/justice_glossary.py
@@ -2,25 +2,20 @@
 
 from typing import Any
 
-from scrapy import Spider
 from scrapy.http.response.html import HtmlResponse
 
 from ...parsers.nzl.justice_glossary import parse_glossary
+from ...models.glossary import GlossaryParseResult
+from ..base import BaseGlossarySpider
 
 JD_VERBOSE_NAME = "New Zealand"
 PUBLICATION_NAME = "Glossary"
 
 
-class NZLJusticeGlossary(Spider):
-    name       = "nzl_justice_glossary"
+class NZLJusticeGlossary(BaseGlossarySpider):
+    name = "nzl_justice_glossary"
     start_urls = ["https://www.justice.govt.nz/about/glossary/"]
 
-    def parse(self, response: HtmlResponse, **_: dict[str, Any]):
-        """Framework callback which begins the parsing.
-
-        @url https://www.justice.govt.nz/about/glossary/
-        @returns items 1 1
-        @returns requests 0 0
-        @scrapes metadata entries
-        """
-        yield parse_glossary(response).asdict()
+    def parse_glossary(self, response: HtmlResponse) -> GlossaryParseResult:
+        """Framework callback which begins the parsing."""
+        return parse_glossary(response)
