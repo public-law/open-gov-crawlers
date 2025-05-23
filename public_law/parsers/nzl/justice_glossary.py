de61aa62c66e5e941271c13f98cd9a4f85687208
Robb Shecter <robb@public.law>
2025-05-23 16:41:23 -0600

    fix: type error

--- public_law/parsers/nzl/justice_glossary.py
+++ public_law/parsers/nzl/justice_glossary.py
@@ -1,6 +1,11 @@
-from typing import Any, Iterable
+from typing import Any, Iterable, cast
 
+from bs4 import Tag
+from bs4.element import NavigableString
 from scrapy.http.response.html import HtmlResponse
+from toolz.functoolz import pipe  # type: ignore
+
+from public_law import text
 
 from ...metadata import Metadata, Subject
 from ...models.glossary import GlossaryEntry, GlossaryParseResult
@@ -8,6 +13,8 @@ from ...text import URL, LoCSubject
 from ...text import NonemptyString as String
 from ...text import (Sentence, ensure_ends_with_period, make_soup,
                      normalize_nonempty)
+from ...html import parse_html, TypedSoup
+from ...result import Result, Ok, Err, cat_oks
 
 
 def parse_glossary(html: HtmlResponse) -> GlossaryParseResult:
@@ -19,7 +26,8 @@ def parse_glossary(html: HtmlResponse) -> GlossaryParseResult:
             dcterms_language="en",
             dcterms_coverage="NZL",
             # Info about original source
-            dcterms_source=String("https://www.justice.govt.nz/about/glossary/"),
+            dcterms_source=String(
+                "https://www.justice.govt.nz/about/glossary/"),
             publiclaw_sourceModified="unknown",
             publiclaw_sourceCreator=String("New Zealand Ministry of Justice"),
             dcterms_subject=(
@@ -43,15 +51,27 @@ def __parse_entries(html: HtmlResponse) -> Iterable[GlossaryEntry]:
     for phrase, defn in __raw_entries(html):
         yield GlossaryEntry(
             phrase=normalize_nonempty(phrase.text),
-            definition=Sentence(normalize_nonempty(ensure_ends_with_period(defn.text))),
+            definition=Sentence(normalize_nonempty(
+                ensure_ends_with_period(defn.text))),
         )
 
 
-def __raw_entries(html: HtmlResponse) -> Iterable[tuple[Any, Any]]:
+def __raw_entries(response: HtmlResponse) -> Iterable[tuple[Any, Any]]:
     """
     The core of this parser.
 
     TODO: Refactor all the glossary parsers to need only this function.
     """
-    soup = make_soup(html)
-    return ((phrase, phrase.parent.next_sibling) for phrase in soup.find_all("strong"))
+    soup = make_soup(response)
+
+    def get_next_sibling(tag: Tag) -> Tag | None:
+        if not tag.parent:
+            return None
+        sibling = tag.parent.next_sibling
+        if isinstance(sibling, Tag):
+            return sibling
+        return None
+
+    return ((phrase, get_next_sibling(phrase))
+            for phrase in soup.find_all("strong")
+            if isinstance(phrase, Tag))
