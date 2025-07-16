eb281f72156703b029dbe0b7db7ec343b52e83a5
Robb Shecter <robb@public.law>
2025-07-16 02:55:46 -0600

    refactor: move glossary spider base files

--- public_law/glossaries/spiders/aus/lawhandbook_glossary.py
+++ public_law/glossaries/spiders/aus/lawhandbook_glossary.py
@@ -1,6 +1,6 @@
 from scrapy.http.response.html import HtmlResponse
 
-from public_law.shared.spiders.enhanced_base import EnhancedAutoGlossarySpider
+from public_law.glossaries.spiders._base.enhanced_base import EnhancedAutoGlossarySpider
 from public_law.shared.models.metadata import Metadata, Subject
 from public_law.shared.utils.text import URL, LoCSubject
 from public_law.shared.utils.text import NonemptyString as String
